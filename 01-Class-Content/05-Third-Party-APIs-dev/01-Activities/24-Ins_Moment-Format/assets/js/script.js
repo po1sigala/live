e9159a9c0a62ef511cc61c6b5cd2a78dc090ee00
markjnkim <mark.jn.kim@gmail.com>
2020-07-28 10:43:59 -0700

    update moment CDN to LTS add activity 28 Instructor demo RTFM

--- 01-Class-Content/05-Third-Party-APIs-dev/01-Activities/24-Ins_Moment-Format/assets/js/script.js
+++ 01-Class-Content/05-Third-Party-APIs-dev/01-Activities/24-Ins_Moment-Format/assets/js/script.js
@@ -1,13 +1,15 @@
 // Print the following answers in the console.
-// TODO: What is today's date in the following format: Jan 1st, 1999?
+// 1. What is today's date in the following format: Jan 1st, 1999?
 var now = moment();
 console.log(now);
 
-// Current Date
-console.log(now.format("MM/DD/YY"));
+console.log(now.format("MM Do, YYYY"));
 
-// Current day
+// 2. What is the day of the week today?
 console.log(now.format("[Today is] dddd"));
 
 // Parse the following date, 11/3/2020, and convert it into any time/date format.
 console.log(moment("11/3/20", "MM-DD-YY").format("dd MMM Do, YYYY, hh:mm:ss"));
+
+// 3. I need to place my recycling bin on the curb on every odd week of the year for collection. Do I need to put out my recycling bin out this week?
+console.log(now.format("w"));  
--- 01-Class-Content/05-Third-Party-APIs-dev/01-Activities/28-Ins_Moment-Functions/assets/js/script.js
+++ 01-Class-Content/05-Third-Party-APIs-dev/01-Activities/28-Ins_Moment-Functions/assets/js/script.js
@@ -1,18 +1,29 @@
-// Print the following answers in the console.
+// Moment Methods
 
-// What day of this year is Christmas?
+// Print the following answers in the console.
+var today = moment();
+// 1. What day out of this year is Christmas? (dayNum of 365)
 var newYearsDay = moment("2020-01-01");
 var christmasDay = moment("20201225");
-var daysDiff = newYearsDay.diff(christmasDay, "days");
-console.log(daysDiff); // 359
+var dayNum = christmasDay.diff(newYearsDay, "days");
+console.log(dayNum); // 359
 
 
-// Programmatically find the 100th day of the current year?
-// 1. Find the start of this year
+// 2. Programmatically find the 100th day of the current year.
+
+// 2a. Find the start of this year
 var startOfThisYear = moment().startOf('year');
-// 2. Find 100 days after the beginning of this year
+
+// 2b. Find 100 days after the beginning of this year
 var hundredDays = startOfThisYear.add("100", "days").format("dddd MMMM Do, YYYY");
 console.log(hundredDays); // Friday April 10th, 2020
 
-// Check if newYearsDay is before Christmas
-newYearsDay.isBefore(christmasDay); // false
+// 4. How much time until I graduate on June 15, 2022?
+console.log(today.to(moment("2022-06-15")));
+
+// 4. Check if the Unix time, 1009872000, is before today, then convert into a date to verify
+console.log(moment("1009872000", "X").isAfter(today)); // false
+
+console.log(moment("1009872000", "X").format("dddd MMMM Do, YYYY"));
+
+
