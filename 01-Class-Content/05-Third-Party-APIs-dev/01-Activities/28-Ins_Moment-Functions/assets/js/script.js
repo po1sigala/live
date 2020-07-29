460a438517b83925b2df088d1069b33d346ab2b1
markjnkim <mark.jn.kim@gmail.com>
2020-07-29 08:49:38 -0700

    add styling to activity 28 Instructor polish questions for activity 28-Ins

--- 01-Class-Content/05-Third-Party-APIs-dev/01-Activities/28-Ins_Moment-Functions/assets/js/script.js
+++ 01-Class-Content/05-Third-Party-APIs-dev/01-Activities/28-Ins_Moment-Functions/assets/js/script.js
@@ -7,6 +7,7 @@ var newYearsDay = moment("2020-01-01");
 var christmasDay = moment("20201225");
 var dayNum = christmasDay.diff(newYearsDay, "days");
 console.log(dayNum); // 359
+$("#1a").text(dayNum);
 
 // 2. Programmatically find the 100th day of the current year.
 // 2a. First select the start of this year
@@ -14,19 +15,24 @@ var startOfThisYear = today.startOf("year");
 
 // 2b. Find 100 days after the beginning of this year
 var hundredDays = startOfThisYear.add("100", "days").format("dddd MMMM Do, YYYY");
-console.log(hundredDays); // Friday April 10th, 2020
+$("#2a").text(hundredDays); // Friday April 10th, 2020
 
-// 2c. Display the variable today in a date format. 
+// Display the variable today in a date format. 
 // How did this change from the original declaration?
-console.log(today.format("dddd MMM Do, YYYY"));
+console.log("Is this still today? ", today.format("dddd MMM Do, YYYY")); // Friday April 10th, 2020
 
-// 3. How much time until I graduate this course? <plug in your grad date>
-console.log(moment().to(moment("2021-06-15"))); // in a year
+// 3. How much time until I graduate this course? (enter in your grad date)
+var gradDate = moment().to(moment("2021-06-15"));
+$("#3a").text(gradDate); // in a year
 
-// 4. Check if the Unix time, 160948800, is after today, then convert into a date to verify.
-console.log(moment("160948800", "X").isAfter(moment())); // false
+// 4. Is the Unix time, 160948800, after today? Format the date to verify.
+// 4a. The date is after today 
+var inFuture = moment("160948800", "X").isAfter(moment());
+$("#4a").text(inFuture); // false
 
-console.log(moment("160948800", "X").format("dddd MMMM Do, YYYY")); // Thursday February 6th, 1975
+// 4b. The date formatted legibly 
+var date = moment("160948800", "X").format("dddd MMMM Do, YYYY"); 
+$("#4a").append("<br>" + date); // Thursday February 6th, 1975
 
 // 5. Calculate how many minutes until the beginning of the next hour.
 // 5a. First select the start of the next hour
@@ -34,4 +40,5 @@ var nextHour = moment().add("1", "hour").startOf("hour");
 
 // console.log(nextHour.format("dddd MMM Do, YYYY [and] HH:mm:ss"));
 
-console.log(nextHour.diff(moment(), "minutes"));
+var minutesTo = nextHour.diff(moment(), "minutes");
+$("#5a").text(minutesTo); 
