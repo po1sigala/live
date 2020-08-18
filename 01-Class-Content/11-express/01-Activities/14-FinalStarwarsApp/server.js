f1b9f8f236147d92e5de8b829c2ebfd143d271fc
John McCambridge <jmccamb@gmail.com>
2020-08-18 14:12:24 -0500

    Update 14 to not use jquery - removed cdn link from activity. Update server to es6"

--- 01-Class-Content/11-express/01-Activities/14-FinalStarwarsApp/server.js
+++ 01-Class-Content/11-express/01-Activities/14-FinalStarwarsApp/server.js
@@ -1,12 +1,12 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require('express');
+const path = require('path');
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,81 +14,70 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
+const characters = [
   {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
+    routeName: 'yoda',
+    name: 'Yoda',
+    role: 'Jedi Master',
     age: 900,
-    forcePoints: 2000
+    forcePoints: 2000,
   },
   {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
+    routeName: 'darthmaul',
+    name: 'Darth Maul',
+    role: 'Sith Lord',
     age: 200,
-    forcePoints: 1200
+    forcePoints: 1200,
   },
   {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
+    routeName: 'obiwankenobi',
+    name: 'Obi Wan Kenobi',
+    role: 'Jedi Master',
     age: 55,
-    forcePoints: 1350
-  }
+    forcePoints: 1350,
+  },
 ];
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
+app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
 
 // Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
-});
+app.get('/api/characters', (req, res) => res.json(characters));
 
 // Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
+app.get('/api/characters/:character', (req, res) => {
+  const chosen = req.params.character;
 
   console.log(chosen);
 
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
+  /* Check each character routeName and see if the same as "chosen"
+   If the statement is true, send the character back as JSON,
+   otherwise send back JSON containing the false boolean */
+  characters.forEach((character) =>
+    chosen === character.routeName ? res.json(character) : res.json(false)
+  );
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
+app.post('/api/characters', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newCharacter = req.body;
+  const newCharacter = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
   console.log(newCharacter);
 
   characters.push(newCharacter);
-
   res.json(newCharacter);
 });
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
