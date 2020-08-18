0b0b593520d6e0ea9b8e5778372204c1d7e224ee
John McCambridge <jmccamb@gmail.com>
2020-08-18 15:18:00 -0500

    fix forEach for characters"

--- 01-Class-Content/11-express/01-Activities/14-FinalStarwarsApp/server.js
+++ 01-Class-Content/11-express/01-Activities/14-FinalStarwarsApp/server.js
@@ -58,9 +58,14 @@ app.get('/api/characters/:character', (req, res) => {
   /* Check each character routeName and see if the same as "chosen"
    If the statement is true, send the character back as JSON,
    otherwise tell the user no character was found */
-  characters.forEach((character) =>
-    chosen === character.routeName ? res.json(character) : res.json(false)
-  );
+
+  for (let i = 0; i < characters.length; i++) {
+    if (chosen === characters[i].routeName) {
+      return res.json(characters[i]);
+    }
+  }
+
+  return res.json(false);
 });
 
 // Create New Characters - takes in JSON input
