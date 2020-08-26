61cf9edf91a92ad4aad44024bb6c83506def7e55
John McCambridge <jmccamb@gmail.com>
2020-08-26 12:46:04 -0500

    MVC example es6 update

--- 01-Class-Content/13-MVC/01-Activities/16-MvcExample/config/connection.js
+++ 01-Class-Content/13-MVC/01-Activities/16-MvcExample/config/connection.js
@@ -1,21 +1,23 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: "localhost",
+const connection = mysql.createConnection({
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "",
-  database: "cat_db"
+  user: 'root',
+  // NOTE: Be sure to add your MySQL password here!
+  //! REMOVE THISSS!!!!!!!
+  password: 'starwars',
+  database: 'cat_db',
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect((err) => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 // Export connection for our ORM to use.
