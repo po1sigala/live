a4da291668aec6956a484d731ffd35d19f385bd0
John McCambridge <jmccamb@gmail.com>
2020-08-31 12:08:24 -0500

    Update 05 activity to use es6

--- 01-Class-Content/14-Full-Stack/01-Activities/05-SequelizeLibrary/Solved/app/config/connection.js
+++ 01-Class-Content/14-Full-Stack/01-Activities/05-SequelizeLibrary/Solved/app/config/connection.js
@@ -1,20 +1,17 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
-
 // Dependencies
-var Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_library", "root", "", {
-  host: "localhost",
+// TODO: Remove your password dummy!
+const sequelize = new Sequelize('sequelize_library', 'root', 'starwars', {
+  host: 'localhost',
   port: 3306,
-  dialect: "mysql",
+  dialect: 'mysql',
   pool: {
     max: 5,
     min: 0,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
 
 // Exports the connection for other files to use
