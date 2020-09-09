fe7bf8099917231b0ef8a68f61c7ed2158d42a85
John McCambridge <jmccamb@gmail.com>
2020-09-09 13:57:56 -0500

    Update activity 20

--- 01-Class-Content/09-NodeJS/01-Activities/20-Stu_Inquirer-Users/Solved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/20-Stu_Inquirer-Users/Solved/index.js
@@ -1,44 +1,30 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name?"
-  },
-  {
-    type: "checkbox",
-    message: "What languages do you know?",
-    name: "stack",
-    choices: [
-      "HTML", 
-      "CSS", 
-      "JavaScript", 
-      "MySQL"
-    ]
-  },
-  {
-    type: "list",
-    message: "What is your preferred method of communication?",
-    name: "contact",
-    choices: [
-      "email",
-      "phone",
-      "telekinesis"
-    ]
-  }
-]).then(function(data) {
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-  var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log("Success!");
-
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
   });
-});
