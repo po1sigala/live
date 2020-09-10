d98306607a950f307f4f41e264dfab045e42307f
John McCambridge <jmccamb@gmail.com>
2020-09-10 12:12:55 -0500

    Update mini project to use arrow functions

--- 01-Class-Content/09-NodeJS/01-Activities/28-Stu_Mini-project/Solved/Basic/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/28-Stu_Mini-project/Solved/Basic/index.js
@@ -1,47 +1,45 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-  return inquirer.prompt([
+const promptUser = () =>
+  inquirer.prompt([
     {
-      type: "input",
-      name: "name",
-      message: "What is your name?"
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
     },
     {
-      type: "input",
-      name: "location",
-      message: "Where are you from?"
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
     },
     {
-      type: "input",
-      name: "hobby",
-      message: "What is your favorite hobby?"
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
     },
     {
-      type: "input",
-      name: "food",
-      message: "What is your favorite food?"
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
     },
     {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
     },
     {
-      type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL."
-    }
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
   ]);
-}
 
-function generateHTML(answers) {
-  return `
-<!DOCTYPE html>
+const generateHTML = (answers) =>
+`<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -63,17 +61,8 @@ function generateHTML(answers) {
 </div>
 </body>
 </html>`;
-}
 
 promptUser()
-  .then(function(answers) {
-    const html = generateHTML(answers);
-
-    return writeFileAsync("index.html", html);
-  })
-  .then(function() {
-    console.log("Successfully wrote to index.html");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+  .then(() => console.log('Successfully wrote to index.html'))
+  .catch((err) => console.error(err));
