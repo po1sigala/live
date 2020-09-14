eb2a8815fbeb881ce9934b8c058ddfc992ec1d9d
John McCambridge <jmccamb@gmail.com>
2020-09-14 09:55:07 -0500

    Update activity 1

--- 01-Class-Content/09-NodeJS/01-Activities/01-Ins_Node-Demo/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/01-Ins_Node-Demo/index.js
@@ -1,2 +1,5 @@
-// run this from the command line using 'node index.js'
-console.log('JavaScript outside the browser?!');
+// Run this from the command line using 'node index.js'
+(function () {
+  console.table('this');
+  console.log(this);
+})();
