e8ebb0b316707925c28dee978897a1b43dfe70a1
Tamar <mischegoss@gmail.com>
2020-09-16 11:13:53 -0400

    Change activity to 'we' language

--- 01-Class-Content/03-JavaScript-dev/01-Activities/13-Ins_Iteration/script.js
+++ 01-Class-Content/03-JavaScript-dev/01-Activities/13-Ins_Iteration/script.js
@@ -1,14 +1,13 @@
-// A for-loop allows you to execute code more than once
-// A for loop that starts with 0, runs while i is less an 5 and increments by 1
+// We use a for-loop to execute code more than once
 for (var i = 0; i < 5; i++) {
-    //This is the block of code that will run each time
+    // This is the block of code that will run each time
     console.log("This is the current value of i: " + i + ".");
 }
 
 // For-loops are often used to iterate over arrays
 var zooAnimals = ["Bears", "Giraffes", "Penguins", "Meerkats"];
 
-// A for-loop that starts with 0, runs while i is less than array length, increments by 1
-for (var i=0; i < zooAnimals.length; i++) { 
+//To determine how many times the loop should execute, we use the array's length
+for (var i = 0; i < zooAnimals.length; i++) { 
     console.log("I am going to zoo to see " + zooAnimals[i] + ".");
 }
