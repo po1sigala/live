aba72af472b03da55bdb6fcdb55c132d3b47a021
sevans924 <sevans924@gmail.com>
2020-09-18 16:12:04 -0400

    Deleted Solved file from 18 and replaced it with solved file from 12

--- 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/models/user.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/models/user.js
@@ -1,41 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// create our User model
 class User extends Model {}
 
-// create fields/columns for User model
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    username: {
-        type: DataTypes.STRING
-    }
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    }
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
-  }
-);
+});
 
 module.exports = User;
