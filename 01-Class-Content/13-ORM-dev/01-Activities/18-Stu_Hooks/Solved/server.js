84060dfe06033da096a38fb3c03d04def2ac805e
sevans924 <sevans924@gmail.com>
2020-09-21 14:56:32 -0400

    added routes to userRoutes.js in 13.18

--- 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/server.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
+
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
