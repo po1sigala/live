42065c1836e026d523bbbc555e8c46c523c1df24
gchoi2u <gchoi@2u.com>
2020-09-21 16:19:15 -0400

    modified 12 activity to use try/catch

--- 01-Class-Content/13-ORM-dev/01-Activities/12-Stu_RESTful-Routes/Solved/models/User.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/12-Stu_RESTful-Routes/Solved/models/User.js
@@ -3,10 +3,27 @@ const sequelize = require('../config/connection');
 
 class User extends Model {}
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
   },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
 
 module.exports = User;
