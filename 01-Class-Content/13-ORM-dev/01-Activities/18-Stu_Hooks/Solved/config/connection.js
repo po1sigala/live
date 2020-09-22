18940d074e8d42718dc8e470d4dc6877bd487e37
gchoi2u <gchoi@2u.com>
2020-09-22 09:01:59 -0400

    fixed User model for 18 activity

--- 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/config/connection.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/config/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
-//require('dotenv').config();
+require('dotenv').config();
 
-let sequelize = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PW,
+  process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
