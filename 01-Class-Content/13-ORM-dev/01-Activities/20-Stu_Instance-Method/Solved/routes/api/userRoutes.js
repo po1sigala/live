5e8e485179fe95c32692c7ad57fc28f73a766d93
sevans924 <sevans924@gmail.com>
2020-09-22 14:44:18 -0400

    Debugging login route, still not working

--- 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Solved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Solved/routes/api/userRoutes.js
@@ -2,6 +2,43 @@ const router = require('express').Router();
 const User = require('../../models/User.js');
 
 
+
+// POST create a new user
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+// POST user login
+  router.post('/login', (req, res) => {
+    // try {
+        const userData = User.findOne({ where: { email: req.body.email } })
+        if (!userData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+        }
+        console.log(userData)
+        const validPassword = userData.checkPassword(req.body.password);
+            if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+            }
+         res.json({ user: userData, message: 'You are now logged in!' });
+        
+      
+    // } catch (err) {
+    //     res.status(400).json(err)
+    // }
+  });
+
 // GET one user
 router.get('/:id', async (req, res) => {
     try {
@@ -17,19 +54,6 @@ router.get('/:id', async (req, res) => {
   });
 
 
-// POST create a new user
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      });
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
 
 // PUT update a user
 router.put('/:id', async (req, res) => {
