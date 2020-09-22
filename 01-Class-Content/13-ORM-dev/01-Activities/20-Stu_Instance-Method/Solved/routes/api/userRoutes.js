26443a183e8a005a7fdd43f6ea3cae54b8bad6b1
sevans924 <sevans924@gmail.com>
2020-09-22 15:21:20 -0400

    Changed error message in userRoutes in bot Solved and Unsolved

--- 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Solved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Solved/routes/api/userRoutes.js
@@ -18,17 +18,17 @@ router.post('/', async (req, res) => {
   });
 
 // POST user login
-  router.post('/login', async(req, res) => {
+router.post('/login', async(req, res) => {
     try {
         const userData = await User.findOne({ where: { email: req.body.email } })
         if (!userData) {
-        res.status(400).json({ message: 'No user with that email address!' });
+        res.status(400).json({ message: 'Incorrect email or password, please try again' });
         return;
         }
         
         const validPassword = await userData.checkPassword(req.body.password);
             if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!' });
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
             }
          res.json({ user: userData, message: 'You are now logged in!' });
--- 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Unsolved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Unsolved/routes/api/userRoutes.js
@@ -17,7 +17,27 @@ router.post('/', async (req, res) => {
     }
   });
 
-// TODO: ADD YOUR CODE BELOW
+// POST user login
+router.post('/login', async(req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } })
+        if (!userData) {
+        res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        return;
+        }
+        
+        const validPassword = await userData.checkPassword(req.body.password);
+            if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+            return;
+            }
+         res.json({ user: userData, message: 'You are now logged in!' });
+        
+      
+    } catch (err) {
+        res.status(400).json(err)
+    }
+  });
 
 
 // GET one user
