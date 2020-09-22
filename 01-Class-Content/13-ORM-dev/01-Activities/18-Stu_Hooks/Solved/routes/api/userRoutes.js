ab5e76c02bc595b933a9becd341a5a36049411df
sevans924 <sevans924@gmail.com>
2020-09-21 20:00:47 -0400

    fixed routes in userRoutes.js

--- 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/18-Stu_Hooks/Solved/routes/api/userRoutes.js
@@ -2,30 +2,35 @@ const router = require('express').Router();
 const User = require('../../models/User.js');
 
 
-// GET /api/users/1
-router.get('/:id', async(req, res) => {
+// GET one user
+router.get('/:id', async (req, res) => {
     try {
-        const userData = await User.findByPk(req.params.id);
-        res.status(200).json(userData);
-    } catch(err) {
-        res.status(404).json(err)
+      const userData = await User.findByPk(req.params.id);
+      if (!userData) {
+        res.status(404).json({ message: 'No user with this id!' });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
     }
+  });
 
-});
 
-
-// POST /api/users
-router.post('/', async(req, res) => {
+// POST create a new user
+router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-        res.status(200).json(userData)
-    } catch(err) {
-        res.status(400).json(err)
+      const userData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
     }
+  });
 
-});
-
-// PUT /api/users/1
+// PUT update a user
 router.put('/:id', async (req, res) => {
     try {
       const userData = await User.update(req.body, {
