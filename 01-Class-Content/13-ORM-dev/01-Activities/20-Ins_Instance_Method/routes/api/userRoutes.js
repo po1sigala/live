2fb5b0897ea6a7f2c5437f4d04209f3740f150f0
sevans924 <sevans924@gmail.com>
2020-09-23 16:18:26 -0400

    added space for instructor demo examples - still TODO

--- 01-Class-Content/13-ORM-dev/01-Activities/20-Ins_Instance_Method/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/20-Ins_Instance_Method/routes/api/userRoutes.js
@@ -17,27 +17,7 @@ router.post('/', async (req, res) => {
     }
   });
 
-// POST user login
-router.post('/login', async(req, res) => {
-    try {
-        const userData = await User.findOne({ where: { email: req.body.email } })
-        if (!userData) {
-        res.status(400).json({ message: 'Incorrect email or password, please try again' });
-        return;
-        }
-        
-        const validPassword = await userData.checkPassword(req.body.password);
-            if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect email or password, please try again' });
-            return;
-            }
-         res.json({ user: userData, message: 'You are now logged in!' });
-        
-      
-    } catch (err) {
-        res.status(400).json(err)
-    }
-  });
+////////////////////TODO Add route for custom instance method//////////////
 
 // GET one user
 router.get('/:id', async (req, res) => {
