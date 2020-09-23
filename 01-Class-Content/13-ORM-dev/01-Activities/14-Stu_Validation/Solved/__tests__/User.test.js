282517f87f89195a593c74d7ab0e368c9eb8b4f6
gchoi2u <gchoi@2u.com>
2020-09-23 14:13:29 -0400

    added unit tests for 14 activity

--- 01-Class-Content/13-ORM-dev/01-Activities/14-Stu_Validation/Solved/__tests__/User.test.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/14-Stu_Validation/Solved/__tests__/User.test.js
@@ -1,49 +1,44 @@
 const sequelize = require('../config/connection');
 const User = require('../models/User.js');
 
-test('Does not accept null values', async () => {
-  const user = {};
+test('Checks for null values', async () => {
+  const user1 = {};
 
-  const newUser = User.create(user);
-
-  const checkUser = (newUser) => {
-    if (!newUser) {
-      throw new Error('User was not created');
-    }
+  const user2 = {
+    username: 'test',
+    email: 'test@test.com',
+    password: '1111111111111',
   };
 
-  test('thorws error'),
-    async () => {
-      expect(() => {
-        checkUser();
-      }).toThrow('User was not created');
+  const newUser1 = User.build(user1);
+  const newUser2 = User.build(user2);
 
-      expect(() => {
-        checkUser();
-      }).not.toThrow();
-    };
+  // expects errors
+  await expect(newUser1.validate()).rejects.toThrow('notNull');
+  // expects no errors
+  await expect(newUser2.validate()).resolves.not.toThrow();
 });
 
-// test('Prevents null values for email', async () => {
-//   const user = {
-//     username: '',
-//     email: '',
-//     password: '',
-//   };
-//   const newUser = User.build(user);
+test('Checks for short passwords', async () => {
+  const user1 = {
+    username: 'test',
+    email: 'test@test.com',
+    password: '123',
+  };
 
-//   console.log(newUser.email);
-//   return expect(newUser.email).toBeTruthy();
-// });
+  const user2 = {
+    username: 'test',
+    email: 'test@test.com',
+    password: 'password123',
+  };
 
-// test('Prevents null values for password', async () => {
-//   const user = {
-//     username: '',
-//     email: '',
-//     password: '',
-//   };
-//   const newUser = User.build(user);
+  const newUser1 = User.build(user1);
+  const newUser2 = User.build(user2);
 
-//   console.log(newUser.password);
-//   return expect(newUser.password).toBeTruthy();
-// });
+  // expects errors
+  await expect(newUser1.validate()).rejects.toThrow(
+    'Validation len on password failed'
+  );
+  // expects no errors
+  await expect(newUser2.validate()).resolves.not.toThrow();
+});
