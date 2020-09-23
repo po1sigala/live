1311031338e9ae95063ee0975a49b779498d4785
gchoi2u <gchoi@2u.com>
2020-09-23 17:18:28 -0400

    modified 16 Stu activity solved code

--- 01-Class-Content/13-ORM-dev/01-Activities/16-Stu_Password-Hashing/Solved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/16-Stu_Password-Hashing/Solved/routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const User = require('../../models/User.js');
 const bcrypt = require('bcrypt');
+const User = require('../../models/User.js');
 
 // GET all users
 router.get('/', async (req, res) => {
@@ -77,20 +77,23 @@ router.delete('/:id', async (req, res) => {
 // TODO: Add comments describing the functionality of this `login` route
 router.post('/login', async (req, res) => {
   try {
+    // we search the DB for a user with the provided email
     const userData = await User.findOne({ where: { email: req.body.email } });
     if (!userData) {
       res.status(404).json({ message: 'No user with this email address!' });
       return;
     }
-
+    // use `bcrypt.compare()` to compare the provided password and the hashed password
     const validPassword = await bcrypt.compare(
       req.body.password,
       userData.password
     );
+    // if they do not match, return error message
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
+    // if they do match, return success message
     res.status(200).json({ message: 'You are now logged in!' });
   } catch (err) {
     res.status(500).json(err);
