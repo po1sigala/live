64071c7fcbf5feba67081d3970296ba63e139d82
sevans924 <sevans924@gmail.com>
2020-09-24 13:55:14 -0400

    Reorganized routes in 17 - 20

--- 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Solved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM-dev/01-Activities/20-Stu_Instance-Method/Solved/routes/api/userRoutes.js
@@ -2,6 +2,19 @@ const router = require('express').Router();
 const User = require('../../models/User.js');
 
 
+// GET one user
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if (!userData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // POST create a new user
 router.post('/', async (req, res) => {
@@ -38,22 +51,6 @@ router.post('/login', async(req, res) => {
         res.status(400).json(err)
     }
   });
-
-// GET one user
-router.get('/:id', async (req, res) => {
-    try {
-      const userData = await User.findByPk(req.params.id);
-      if (!userData) {
-        res.status(404).json({ message: 'No user with this id!' });
-        return;
-      }
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-
 
 // PUT update a user
 router.put('/:id', async (req, res) => {
