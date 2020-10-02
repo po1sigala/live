c9bad28e19643cfe6e5442b1839570178cd1ae8b
Tamar <mischegoss@gmail.com>
2020-10-02 13:59:39 -0400

    Refactor Traversal, remove README

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/03-Ins_Traverse-DOM/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/03-Ins_Traverse-DOM/script.js
@@ -1,8 +1,15 @@
+console.log("Document Body: ")
 console.log(document.body);
 
+console.log("Children of Document Body: ")
 console.log(document.body.children);
 
+console.log("First child of document body")
 console.log(document.body.children[1]);
 
+console.log("First child node")
 console.log(document.body.children[1].childNodes[1]);
+
+var childNode1 = document.getElementById("childNode1")
+console.log(childNode1);
 
