6cd8f16241fd7d2f82e59dac699f745da1a31342
Tamar <mischegoss@gmail.com>
2020-10-02 11:33:25 -0400

    Activity 1 Refactor

---
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/01-Ins_Window-Object/script.js
@@ -0,0 +1,11 @@
+// Logs window object using this
+console.log("this: ");
+console.log(this);
+
+// Logs window object using document.defaultView
+console.log("document.defaultView: ");
+console.log(document.defaultView);
+
+// Logs body of HTML using document.body
+console.log("document.body: ");
+console.log( document.body);
