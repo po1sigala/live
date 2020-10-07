3402e87f7073c33dcceab720d59c67e4fe963607
Tamar <mischegoss@gmail.com>
2020-10-07 12:44:53 -0400

    Fix Act 3 image

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/05-Ins_Setting-Attributes/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/05-Ins_Setting-Attributes/script.js
@@ -10,7 +10,7 @@
  pTags[0].setAttribute("style", "font-size: 40px;");
  changeP.setAttribute("style", "font-size: 25px; font-weight: bold; text-decoration:underline; ");
  aTags[0].setAttribute("href", "https://github.com");
- imgEl[0].setAttribute("src", "images/image_1.jpg");
+ imgEl[0].setAttribute("src", "images/image_1.png");
  imgEl[0].setAttribute("style", "width:500px; height:200px;");
 
 for (var i = 0; i < divTags.length; i++) {
