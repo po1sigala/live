ab61bcf32e9e5f0caf8941dbba3975ad86d3c6ce
Tamar <mischegoss@gmail.com>
2020-10-13 23:02:20 -0400

    Add high contrast color palette

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/17-Ins_Event-Bubbling/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/17-Ins_Event-Bubbling/script.js
@@ -6,25 +6,25 @@ var button = document.querySelector(".button");
 function changeBlue(event) {
   event.currentTarget.setAttribute(
     "style",
-    "background-color: blue"
+    "background-color: #63ACBE"
   );
 }
 
 function changePurple(event) {
   event.currentTarget.setAttribute(
     "style",
-    "background-color: purple"
+    "background-color: #601A4A"
   );
 }
 
 function changeOrange(event) {
   event.currentTarget.setAttribute(
     "style",
-    "background-color: orange"
+    "background-color: #EE442F"
   );
 }
 
-outer.addEventListener("click", changeOrange);
-inner.addEventListener("click", changePurple);
+outer.addEventListener("click", changePurple);
+inner.addEventListener("click", changeOrange);
 button.addEventListener("click", changeBlue);
 
