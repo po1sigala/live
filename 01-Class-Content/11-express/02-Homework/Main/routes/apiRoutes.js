738a8495689c1cf089322cfd390d3ae12c99dd37
John McCambridge <jmccamb@gmail.com>
2020-10-15 12:41:04 -0500

    Remove jquery from homework starter

--- 01-Class-Content/11-express/02-Homework/Main/routes/apiRoutes.js
+++ 01-Class-Content/11-express/02-Homework/Main/routes/apiRoutes.js
@@ -5,7 +5,9 @@ const store = require('../db/store');
 router.get('/notes', (req, res) => {
   store
     .getNotes()
-    .then((notes) => res.json(notes))
+    .then((notes) => {
+      return res.json(notes);
+    })
     .catch((err) => res.status(500).json(err));
 });
 
