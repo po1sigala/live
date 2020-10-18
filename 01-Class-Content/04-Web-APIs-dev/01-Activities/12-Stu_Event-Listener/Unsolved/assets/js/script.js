c1785a97b31d2e7fa4ca59b5fe04f96468892855
Tamar <mischegoss@gmail.com>
2020-10-18 12:12:49 -0400

    Update language of Activity 1,rename to Event Listener

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -7,14 +7,4 @@ function setCounterText() {
   countEl.textContent = count;
 }
 
-incrementEl.addEventListener("click", function() {
-  count++;
-  setCounterText();
-});
-
-decrementEl.addEventListener("click", function() {
-  if(count > 0) {
-    count--;
-    setCounterText();
-  }
-}); 
+// Write Your Code Below
--- 01-Class-Content/04-Web-APIs-dev/01-Activities/13-Ins_Preventing-Default/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/13-Ins_Preventing-Default/assets/js/script.js
@@ -4,7 +4,7 @@ var emailInput = document.querySelector("#email");
 var submissionResponseEl = document.querySelector("#response");
 
 submitEl.addEventListener("click", function(event) {
-  event.preventDefault();
+  //  event.preventDefault();
   console.log(event);
   var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
   submissionResponseEl.textContent = response;
