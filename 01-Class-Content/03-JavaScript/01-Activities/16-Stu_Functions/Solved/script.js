6b50b37d7d551b2f367f2fbcdeb84f33a70f69ea
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2020-10-26 11:26:34 -0400

    fix(03-js): adjust user story in activity 16

--- 01-Class-Content/03-JavaScript/01-Activities/16-Stu_Functions/Solved/script.js
+++ 01-Class-Content/03-JavaScript/01-Activities/16-Stu_Functions/Solved/script.js
@@ -1,31 +1,31 @@
-//isEqual using function declaration
-function isEqual(x,y) {
-   if (x === y) {
-      console.log("They are equal in type and value");
-    } else if (x == y) {
-       console.log("They are equal in value");
-    } else {
-      console.log("They are not equal");
-    }
-    return;
+// isEqual using function declaration
+function isEqual(x, y) {
+  if (x === y) {
+    console.log('They are equal in type and value');
+  } else if (x == y) {
+    console.log('They are equal in value');
+  } else {
+    console.log('They are not equal');
+  }
+  return;
 }
 
-//Logs "They are equal in type and value"
+// Logs "They are equal in type and value"
 isEqual(10, 10);
 
 // Logs "They are equal in value"
-isEqual("10",10);
+isEqual('10', 10);
 
 // Using function expression
-var isEqualTakeTwo = function (x,y) {
-    if (x === y) {
-       console.log("They are equal in type and value");
-    } else if (x == y) {
-      console.log("They are equal in value");
-    } else {
-      console.log("They are not equal");
-    }
-    return;
+var isEqualTakeTwo = function (x, y) {
+  if (x === y) {
+    console.log('They are equal in type and value');
+  } else if (x == y) {
+    console.log('They are equal in value');
+  } else {
+    console.log('They are not equal');
+  }
+  return;
 };
 
 // Logs "They are not equal"
