b00e4ed252e9025072b28eb173191538fa64c764
Clark <cnielsen@trilogyed.com>
2020-10-30 13:23:40 -0700

    routes

--- 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/dashboardRoutes.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const { Project } = require('../models');
-
-module.exports = router;
--- 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/homeRoutes.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/homeRoutes.js
@@ -1,8 +1,67 @@
 const router = require('express').Router();
-const { Project } = require('../models');
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-  res.render('homepage', {});
+router.get('/', async (req, res) => {
+  try {
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', { projects });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('project', project);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log(user);
+
+    res.render('profile', user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
