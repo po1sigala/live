cd69e080ad82069406a7205f6add448168ca494d
sevans924 <sevans924@gmail.com>
2020-10-30 14:25:02 -0400

    14.1.4 Unsolved complete

---
+++ 01-Class-Content/14-MVC-dev/01-Activities/04-Stu_Handlebars-Expressions/Unsolved/models/Dish.js
@@ -0,0 +1,42 @@
+/* Note: We are not actually using Sequelize in this activity for simplicity's sake. This file is set up as a preview for when
+you will be including Sequelize in your application*/
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dish extends Model {}
+
+Dish.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    dish_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    guest_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    has_nuts: {
+      type: DataTypes.BOOLEAN,
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'dish'
+  }
+);
+
+module.exports = Dish;
+
