ffbc58555ab6ca2a35c1260eadf05300eb6d8f68
Tamar <mischegoss@gmail.com>
2020-10-30 18:21:43 -0400

    Add comments to map out changes needed

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/27-Stu-Mini-Project/Solved/assets/js/script-old.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/27-Stu-Mini-Project/Solved/assets/js/script-old.js
@@ -1,9 +1,75 @@
-//  TODO: Add array to store JavaScript vocab words
+// Creating a giant wordGuessGame object that will house all of our logic and variables.
+var wordGuessGame = {
 
-
-
-// TODO: Refactor this as init() to mirror previous activities 
-
+    // Object of all words that can be chosen, along with info such as their picture and a song clip.
+    wordsToPick: {
+      genesis: {
+        picture: "genesis.jpg",
+        song: "Illegal Alien",
+        preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+      },
+      madonna: {
+        picture: "madonna.jpg",
+        song: "Material Girl",
+        preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+      },
+      toto: {
+        picture: "toto.jpg",
+        song: "Rosanna",
+        preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+      },
+      queen: {
+        picture: "queen.jpg",
+        song: "Princes of the Universe",
+        preview: "https://p.scdn.co/mp3-preview/b84f24300476f3d3f20056d2388cc51db2e3891f"
+      },
+      u2: {
+        picture: "u2.jpg",
+        song: "With or Without You",
+        preview: "https://p.scdn.co/mp3-preview/28365dff1890075c1371628371cd0e5bbff9a3a3"
+      },
+      metallica: {
+        picture: "metallica.jpg",
+        song: "Master of Puppets",
+        preview: "https://p.scdn.co/mp3-preview/60e6f8dab43f176dd9fb5e795d4e6459bad52e9e"
+      },
+      journey: {
+        picture: "journey.jpg",
+        song: "Don't Stop Believin'",
+        preview: "https://p.scdn.co/mp3-preview/21b9abd3cd2eea634e17a917196fdd5ba2e82670"
+      },
+      inxs: {
+        picture: "inxs.jpg",
+        song: "Need You Tonight",
+        preview: "https://p.scdn.co/mp3-preview/61b17a335d5afc1c4086b1b08e2400f0da147977"
+      },
+      poison: {
+        picture: "poison.jpg",
+        song: "Fallen Angel",
+        preview: "https://p.scdn.co/mp3-preview/0365ad1f152f1834b42b857c4625191cebf9f987"
+      },
+      rush: {
+        picture: "rush.jpg",
+        song: "Limelight",
+        preview: "https://p.scdn.co/mp3-preview/154987dfb07f2fc5ed7aa4d76b80c5dc08ff4d6b"
+      },
+      blondie: {
+        picture: "blondie.jpg",
+        song: "Call Me",
+        preview: "https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90"
+      }
+    },
+  
+    // Variables that set the initial state of our wordGuess game.
+    wordInPlay: null,
+    lettersOfTheWord: [],
+    matchedLetters: [],
+    guessedLetters: [],
+    guessesLeft: 0,
+    totalGuesses: 0,
+    letterGuessed: null,
+    wins: 0,
+  
     // The setupGame method is called when the page first loads.
     setupGame: function() {
       // Here we pick a random word.
@@ -106,7 +172,7 @@
       document.querySelector("#current-word").innerHTML = wordView;
     },
   
- // TODO: Refactor this to use function declaration, simplify
+    // Function that "restarts" the game by resetting all of the variables.
     restartGame: function() {
       document.querySelector("#guessed-letters").innerHTML = "";
       this.wordInPlay = null;
@@ -120,9 +186,6 @@
       this.rebuildWordView();
     },
   
-  // TODO: Add timer from timer exercise, mirror code
-
-
     // Function that checks to see if the user has won.
     updateWins: function() {
       var win;
@@ -182,7 +245,6 @@
   // Initialize the game when the page loads.
   wordGuessGame.setupGame();
   
-  // TODO: Refactor this to match 
   // When a key is pressed..
   document.onkeyup = function(event) {
     // Check if the key pressed is a letter.
