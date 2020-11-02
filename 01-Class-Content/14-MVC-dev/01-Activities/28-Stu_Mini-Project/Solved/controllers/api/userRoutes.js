34467553829bcf5cfc207d20d6a7a64a092d6dbd
Clark <cnielsen@trilogyed.com>
2020-11-02 09:16:46 -0800

    sessions and FE forms

--- 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/api/userRoutes.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/api/userRoutes.js
@@ -4,7 +4,13 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-    res.status(200).json(userData);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
   } catch (err) {
     res.status(400).json(err);
   }
@@ -30,9 +36,25 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    res.json({ user: userData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 
--- 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/homeRoutes.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/28-Stu_Mini-Project/Solved/controllers/homeRoutes.js
@@ -15,7 +15,10 @@ router.get('/', async (req, res) => {
 
     const projects = projectData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', { projects });
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -34,7 +37,10 @@ router.get('/project/:id', async (req, res) => {
 
     const project = projectData.get({ plain: true });
 
-    res.render('project', project);
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -43,13 +49,16 @@ router.get('/project/:id', async (req, res) => {
 router.get('/profile', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
       include: [{ model: Project }],
     });
 
     const user = userData.get({ plain: true });
-    console.log(user);
 
-    res.render('profile', user);
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -57,7 +66,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/profile');
     return;
   }
 
