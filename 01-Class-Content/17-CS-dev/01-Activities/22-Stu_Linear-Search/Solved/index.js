91481e4dc03e584c2838f58bee2fa7e5e2054f3e
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2020-11-02 16:05:01 -0700

    17.3 student activities - git activity

---
+++ 01-Class-Content/17-CS-dev/01-Activities/22-Stu_Linear-Search/Solved/index.js
@@ -0,0 +1,42 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
+
+function linearSearch(array, element) {
+  // Loop the the given array.
+  for (let index = 0; index < array.length; index++) {
+    // Check to see if the index of the given array is equal to the element we are searching for.
+    if (array[index] === element) {
+      // Exit the search by returning the index of the element.
+      return index;
+    }
+  }
+  // -1 when no matching element was found in the given array.
+  return -1;
+}
+
+console.log(linearSearch(arr, 77));
+
+// BONUS answer:
+function linearSearchMultipleElements(array, element) {
+  // Create an empty array to store the indexes of the matching elements.
+  const elemIndexes = [];
+
+  // Loop the the given array.
+  for (let index = 0; index < array.length; index++) {
+    // Check to see if the index of the given array is equal to the element we are searching for.
+    if (array[index] === element) {
+      // Pushes the index of the element we are searching for to the array we created.
+      elemIndexes.push(index);
+    }
+  }
+
+  // Before returning the array of indexes, we first need to check if we have collected any.
+  if (elemIndexes.length === 0) {
+    return 'Not found';
+  } else {
+    return elemIndexes;
+  }
+}
+
+console.log(linearSearchMultipleElements(arr, 77));
+
+module.exports = { linearSearch, linearSearchMultipleElements };
---
+++ 01-Class-Content/17-CS-dev/01-Activities/22-Stu_Linear-Search/Unsolved/index.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
+
+// TODO: Complete the linearSearch function below.
+function linearSearch(array, element) {
+  
+}
