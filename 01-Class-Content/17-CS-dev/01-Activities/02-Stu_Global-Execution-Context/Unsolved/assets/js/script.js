eee047e0ce6ed8a03f197addaf9a45fe469cb6c0
markjnkim <markjnkim@gmail.com>
2020-11-02 12:22:31 -0800

    add Unsolved

---
+++ 01-Class-Content/17-CS-dev/01-Activities/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
@@ -0,0 +1,27 @@
+// Follow the thread of execution ordered by number and answer the following questions.
+
+// 1) Where is the variable, greeting, stored?
+// 
+const greeting = 'Hello';
+
+// 2) Where is sayHello stored?
+// 
+function sayHello() {
+  // 5) Where is the variable, body, stored?
+  // 
+  const body = 'World';
+
+  // 6) Explain what happens in this statement.
+  // Hint: Name the data structures in the correct order.
+  // 
+  console.log(`${greeting} ${body}`);
+}
+
+// 3) Explain what happens in this statement.
+// Hint: Name the data structures in the correct order.
+// 
+alert("What's up doc?");
+
+// 4) What happens with this statement?
+// 
+sayHello();
