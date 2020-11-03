935511fcee9c0cc911394ff4986e7104557d881d
gchoi2u <gchoi@2u.com>
2020-11-02 22:00:08 -0500

    added routes

--- 01-Class-Content/14-MVC-dev/01-Activities/20-Stu_Middleware/Solved/controllers/api/user-routes.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/20-Stu_Middleware/Solved/controllers/api/user-routes.js
@@ -1,77 +1,70 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET all users
-router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] }
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
 // GET one user
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    },
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+router.get('/:id', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id,
+      },
     });
+
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // CREATE new user
-router.post('/', (req, res) => {
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(dbUserData => {
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-  
-        res.json(dbUserData);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
     });
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Login
-router.post('/login', (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
       return;
     }
 
-    const validPassword = dbUserData.checkPassword(req.body.password);
+    const validPassword = await dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
       return;
     }
 
@@ -79,10 +72,15 @@ router.post('/login', (req, res) => {
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
     });
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Logout
--- 01-Class-Content/14-MVC-dev/01-Activities/20-Stu_Middleware/Solved/controllers/home-routes.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/20-Stu_Middleware/Solved/controllers/home-routes.js
@@ -1,21 +1,58 @@
+/* eslint-disable no-case-declarations */
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Gallery, Painting, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET all galleries for homepage
-router.get('/', (req, res) => {
-  Gallery.findAll({})
-  .then(dbGalleryData => {
-    const galleries = dbGalleryData.map(gallery => gallery.get({ plain: true }));
+router.get('/', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findAll({});
+
+    const galleries = dbGalleryData.map((gallery) =>
+      gallery.get({ plain: true })
+    );
     res.render('homepage', {
-      galleries
-    })
-  })
-  .catch(err => {
+      galleries,
+    });
+  } catch (err) {
     console.log(err);
-    res.status(500).json(err)
-  })
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/gallery/:id', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: ['title'],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    res.render('gallery', { gallery });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+
+    res.render('painting', { painting });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
