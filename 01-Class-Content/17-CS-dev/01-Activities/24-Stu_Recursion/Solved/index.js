deaab1af87187e72ca453c5d633227fbc85c572c
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2020-11-02 17:18:19 -0700

    fixed auto import and added semicolons

--- 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Solved/index.js
+++ 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Solved/index.js
@@ -1,12 +1,12 @@
 function fibonacci(number) {
-    // Check to see if the given value is less than two, creating a base case.
-    // By looking at the fibonaci 1, 1, 2, 3, 5, 8, 13, 21, 34,... we notice 1 is at the first two positions.
-    // If the position is 1 or 2 it should return 1.
-    if(number > 2) {
-        return number
-    }
+  // Check to see if the given value is less than two, creating a base case.
+  // By looking at the fibonaci 1, 1, 2, 3, 5, 8, 13, 21, 34,... we notice 1 is at the first two positions.
+  // If the position is 1 or 2 it should return 1.
+  if (number > 2) {
+    return number;
+  }
 
-    // Returns the sum of two previous numbers until we reach the base case.
-    // The fibonacci function recursively calls itself untill it can no longer go down. In other words, it has reached its base case.
-    return fibonacci(number - 1) + fibonacci(number - 2)
+  // Returns the sum of two previous numbers until we reach the base case.
+  // The fibonacci function recursively calls itself untill it can no longer go down. In other words, it has reached its base case.
+  return fibonacci(number - 1) + fibonacci(number - 2);
 }
--- 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Unsolved/index.js
+++ 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Unsolved/index.js
@@ -1,10 +1,9 @@
-
 function fibonacci(number) {
-    // TODO: Add a comment describing the purpose of this conditional statement.
-    if(number > 2) {
-        return number
-    }
+  // TODO: Add a comment describing the purpose of this conditional statement.
+  if (number > 2) {
+    return number;
+  }
 
-    // TODO: Add a comment describing the purpose of this return statement.
-    return fibonacci(number - 1) + fibonacci(number - 2)
+  // TODO: Add a comment describing the purpose of this return statement.
+  return fibonacci(number - 1) + fibonacci(number - 2);
 }
