e5d27ac9c1773b3dc5338f59033850eb7c50799f
sevans924 <sevans924@gmail.com>
2020-11-03 15:03:25 -0500

    Hook up sequelize

--- 01-Class-Content/14-MVC-dev/01-Activities/08-Stu_Data-Serialization/Solved/server.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/08-Stu_Data-Serialization/Solved/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('./controllers/dish-routes'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
