dcebc0f398842c20f98db039395ed2f93ff83023
Tamar <mischegoss@gmail.com>
2020-11-05 17:21:14 -0500

    UI done, logic updated

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/27-Stu-Mini-Project/Solved/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/27-Stu-Mini-Project/Solved/assets/js/script.js
@@ -7,36 +7,62 @@ var winCounter = 0;
 var lossCounter = 0;
 var words = [];
 
-// This reinforces the idea of a function as a reusable block of code
+var button = document.querySelector(".button");
+
 function startGame() {
-  getWords()
+   getWords()
   renderBlanks()
   storeWords();
 }
 
-// This reinforces storing array in local storage
+function winGame() {
+  document.querySelector(".word-blanks").innerHTML = "YOU WON!!!🏆 ";
+  winCounter++
+  updateWins()
+}
+
+function loseGame() {
+  document.querySelector(".word-blanks").innerHTML = "GAME OVER ";
+  lossCounter++
+  updateLosses()
+}
+
+function resetGame() {
+  document.querySelector(".word-blanks").innerHTML = "J _ v _ S c r_ _t";
+  winCounter = 0;
+ lossCounter = 0;
+
+}
+
+function updateWins() {
+  return document.querySelector(".win").innerHTML = winCounter
+}
+
+function updateLosses() {
+  return document.querySelector(".losses").innerHTML = winCounter
+}
+
 function getWords() {
   var storedWords = JSON.parse(localStorage.getItem("storedWords"));
 
   if (storedWords === null) {
     words = ["variable","array","object","string","boolean","function","modulus"];
-    console.log("new array")
   } else {
     words = storedWords;
   }
 }
 
-// This code is actually seen in  prior exercise
 function storeWords() {
   return localStorage.setItem("storedWords", JSON.stringify(words));
 }
 
-// They have seen Math.random and similar functionality last week
 function renderBlanks() {
+  console.log(words)
   chosenWord = words[Math.floor(Math.random() * words.length)];
   lettersInChosenWord = chosenWord.split("");
   numBlanks = lettersInChosenWord.length;
   blanksLetters = [];
+  console.log(words)
 
   for (var i = 0; i < numBlanks; i++) {
     blanksLetters.push("_");
@@ -57,18 +83,11 @@ function checkLetters(letter) {
         blanksLetters[j] = letter;
       } 
     }
-    // Since writing code once is emphasized, this can be made DRY-er.
    document.querySelector(".word-blanks").innerHTML = blanksLetters.join(" ")
   }
 }
 
-// This uses event listener and keydown to match prior activities
 document.addEventListener("keydown", function(event) {
-  if (words.length === 0) {
-    startGame()
-  } else {
-  // They see this exact code in an instructor demo and similar code in activity
-  // .code could also be used but this is what was used before. 
   var key = event.key.toLowerCase();
   var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
   
@@ -76,11 +95,10 @@ document.addEventListener("keydown", function(event) {
     
     var letterGuessed = event.key.toLowerCase();
     checkLetters(letterGuessed)
-  }
-}
-  
+  } 
+
+
 });
 
-// TODO: Add timer functionality and wins/losses functionality with local storage
+button.addEventListener("click", startGame);
 
-// ?? Do we need to show letters that are guessed but wrong?
