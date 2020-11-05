a3ecf09deb90d78dd076761aafa88e29c187d0df
sevans924 <sevans924@gmail.com>
2020-11-05 11:20:30 -0500

    14.1.7 Ins Demo complete

---
+++ 01-Class-Content/14-MVC-dev/01-Activities/07-Ins_Data-Serialization/controllers/dish-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const Dish = require('../models/Dish');
+
+
+router.get('/:id', (req, res) => {
+  Dish.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'dish_name', 'description', 'guest_name', 'has_nuts'],
+  })
+    .then((dbDishData) => {
+      const dish = dbDishData.get({ plain: true });
+      res.render('dish', dish);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
