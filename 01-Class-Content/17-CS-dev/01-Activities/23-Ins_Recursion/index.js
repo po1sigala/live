d3d57e9842758a3ddb2270bee3635811a7cc4ddd
markjnkim <markjnkim@gmail.com>
2020-11-06 11:27:43 -0800

    add 23-Ins-Recursion

--- 01-Class-Content/17-CS-dev/01-Activities/23-Ins_Recursion/index.js
+++ 01-Class-Content/17-CS-dev/01-Activities/23-Ins_Recursion/index.js
@@ -1,8 +1,11 @@
 const countdown = value => {
-  if (value > 0) {
+  // for all values more that 0, logs the value then calls the function with a smaller argument
+  if (value > 0) { 
       console.log(value);
+      // recursive call creates the loop
       return countdown(value - 1);
-  } else {
+    
+  } else { // base condition stops the recursive call
       return value;
   }
 };
