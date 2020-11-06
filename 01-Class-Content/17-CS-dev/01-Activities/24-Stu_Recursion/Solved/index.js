820b69411070c9fa8aa6b255ada9a81e7c03fad6
markjnkim <markjnkim@gmail.com>
2020-11-06 10:55:41 -0800

    polish comments

--- 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Solved/index.js
+++ 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Solved/index.js
@@ -1,12 +1,13 @@
+// the Fibonacci sequence 1 1 2 3 5 8 13 21 34 55 89
 function fibonacci(number) {
-  // Check to see if the given value is less than two, creating a base case.
-  // By looking at the fibonaci 1, 1, 2, 3, 5, 8, 13, 21, 34,... we notice 1 is at the first two positions.
-  // If the position is 1 or 2 it should return 1.
-  if (number > 2) {
-    return number;
+  // The base case or exit condition is required for all recursive functions
+  // Ensures terms 0 and 1 will return 1 as shown in the sequence above
+  if (number <= 1) {
+    return 1;
   }
 
-  // Returns the sum of two previous numbers until we reach the base case.
-  // The fibonacci function recursively calls itself untill it can no longer go down. In other words, it has reached its base case.
+  // The current term is the sum of the previous two terms.
   return fibonacci(number - 1) + fibonacci(number - 2);
 }
+
+module.exports = fibonacci;
--- 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Unsolved/index.js
+++ 01-Class-Content/17-CS-dev/01-Activities/24-Stu_Recursion/Unsolved/index.js
@@ -1,3 +1,5 @@
+// In the fibonacci sequence the current term is the sum of its two previous terms
+// 1 1 2 3 5 8 13 21 34 55 89 
 function fibonacci(number) {
   // TODO: Add a comment describing the purpose of this conditional statement.
   if (number > 2) {
@@ -7,3 +9,5 @@ function fibonacci(number) {
   // TODO: Add a comment describing the purpose of this return statement.
   return fibonacci(number - 1) + fibonacci(number - 2);
 }
+
+module.exports = fibonacci;
