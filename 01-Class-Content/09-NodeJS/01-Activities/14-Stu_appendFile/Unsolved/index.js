f15185b0597b0cf81c996421a16c89f31968eb48
gchoi2u <gchoi@2u.com>
2020-11-09 12:02:41 -0500

    added expalanation of ternary operator

--- 01-Class-Content/09-NodeJS/01-Activities/14-Stu_appendFile/Unsolved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/14-Stu_appendFile/Unsolved/index.js
@@ -3,5 +3,6 @@ const fs = require('fs');
 
 // TODO: Add comments to explain each of the three arguments of appendFile()
 fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
+  // TODO: Describe how this ternary operator works
   err ? console.error(err) : console.log('Commit logged!')
 );
