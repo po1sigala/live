29af083005b0e4c26733bde4e5d16c1873c87506
sevans924 <sevans924@gmail.com>
2020-11-09 15:44:48 -0500

    09.08 README and Activities refactored

--- 01-Class-Content/09-NodeJS/01-Activities/08-Stu_Functional-Loops/Solved/map.js
+++ 01-Class-Content/09-NodeJS/01-Activities/08-Stu_Functional-Loops/Solved/map.js
@@ -1,22 +1,30 @@
 const originalArray = [1, 3, 2, 5, 10];
 
+//doubledArray is equal to a new array of numbers multiplied by 2 and returned by map(). map() will use an anonymous function as a condition. 
 const doubledArray = originalArray.map(function(data) {
+  //The anonymous function returns each number multiplied by 2. The returned result is added to a new array.
   return data * 2;
 });
 
 console.log(doubledArray);
 console.log(originalArray);
 
-// Complete the following
 
-// 1. A map that triples the originalArray and sets the result equal to a new `tripledArray` array
-
+//Describe how map is working in the example below. What will the value of tripledArray be?
+//A map loops through each number in originalArray, multiplies it by 3, and adds it to a new array. The value of tripledArray will be the new array returned by the map.
+//[3, 9, 6, 15, 30]
 const tripledArray = originalArray.map(data => data * 3);
 
-// 2. A map that takes the originalArray and returns a new array `oddOrEven`
+//Describe how map is working in the example below. What will the value of oddOrEven be?
+// A map takes the originalArray, uses a conditional statement to check if a number is even or odd, and returns a new array 
 // containing the text "even" if the number is even and the text "odd" if the number is odd
-// i.e. using this map, the array [3, 1, 2, 4] should give us back ["odd", "odd", "even", "even"]
+// ["odd", "odd", "even", "odd", "even"]
 
-const oddOrEven = originalArray.map(num => (num % 2 === 0 ? "even" : "odd"));
+const oddOrEven = originalArray.map(num => {
+  if (num % 2 === 0) {
+    return 'even'
+  } else {
+    return 'odd'
+  }
+});
 
-// Bonus: Use arrow functions as callbacks!
