a28316d559c01d46e6afa7c757a62aa318b922fa
sevans924 <sevans924@gmail.com>
2020-11-12 16:54:26 -0500

    14.1.1 - 3

--- 01-Class-Content/14-MVC-dev/01-Activities/02-Stu_MVC-Handlebars-Intro/Solved/controllers/dish-routes.js
+++ 01-Class-Content/14-MVC-dev/01-Activities/02-Stu_MVC-Handlebars-Intro/Solved/controllers/dish-routes.js
@@ -1,12 +1,14 @@
-// Is this a Model, a View, or a Controller? What it is responsible for handling?
-// This file is a Controller. It routes commands to the Model and View parts.
+// Is this a Model, a View, or a Controller? 
+// This file is a Controller. 
+// What it is responsible for handling?
+//It routes commands to the Model and View parts.
 
 const router = require('express').Router();
 
-// Add a comment describing the purpose of this route
+// Add a comment describing the purpose of the 'get' route
 // GET route for getting all of the dishes that are on the menu
 router.get('/', async (req, res) => {
-  // Add a comment describing the purpose of this method
+  // Add a comment describing the purpose of the render method
   // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
   res.render('all');
 });
