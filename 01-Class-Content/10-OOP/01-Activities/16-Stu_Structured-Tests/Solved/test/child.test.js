579a72ce6332db064ac33fed88b740d0f841085d
Clark <cnielsen@trilogyed.com>
2020-11-13 16:39:11 -0800

    convert to RE

--- 01-Class-Content/10-OOP/01-Activities/16-Stu_Structured-Tests/Solved/test/child.test.js
+++ 01-Class-Content/10-OOP/01-Activities/16-Stu_Structured-Tests/Solved/test/child.test.js
@@ -1,24 +1,31 @@
 const Child = require("../child");
 
 describe("Child", () => {
+  // Test for all use cases when initializing a new Child object
   describe("Initialization", () => {
     it("should create an object with a name and age if provided valid arguments", () => {
       const child = new Child("Sarah", 3);
 
+      // Verify that the new object has the correct properties
       expect(child.name).toEqual("Sarah");
       expect(child.age).toEqual(3);
     });
 
     it("should throw an error if provided no arguments", () => {
+      // Wrap the object initialization in a callback function that Jest will run
       const cb = () => new Child();
 
+      // Verify that an error was thrown in the callback function
       expect(cb).toThrow();
     });
 
     it("should throw an error if not provided an age", () => {
       const cb = () => new Child("Sarah");
+
+      // Define the error message that is expected to be thrown
       const err = new Error("Expected parameter 'age' to be a non-negative number");
 
+      // Verify that the correct error was thrown when the callback is executed
       expect(cb).toThrowError(err);
     });
 
