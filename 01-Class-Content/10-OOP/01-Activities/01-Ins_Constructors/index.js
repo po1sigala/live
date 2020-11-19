2636c679cb3352c4b82d68c967f6a23b2e9ccee6
gchoi2u <gchoi@2u.com>
2020-11-19 13:46:29 -0500

    refactored 01 02 activity and modified 10.1 PT LP

--- 01-Class-Content/10-OOP/01-Activities/01-Ins_Constructors/index.js
+++ 01-Class-Content/10-OOP/01-Activities/01-Ins_Constructors/index.js
@@ -10,20 +10,20 @@ function Animal(raining, noise) {
 }
 
 // Sets the variables "dogs" and "cats" to be animal objects and initializes them with raining and noise properties
-const dogs = new Animal(true, "Woof!");
-const cats = new Animal(false, "Meow!");
+const dogs = new Animal(true, 'Woof!');
+const cats = new Animal(false, 'Meow!');
 
 // Calling dogs and cats makeNoise methods
 dogs.makeNoise();
 cats.makeNoise();
 
 // If we want, we can change an objects properties after they're created
-cats.raining = true;
-cats.makeNoise();
+// cats.raining = true;
+// cats.makeNoise();
 
 const massHysteria = (dogs, cats) => {
   if (dogs.raining === true && cats.raining === true) {
-    console.log("DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!");
+    console.log('DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!');
   }
 };
 
---
+++ 01-Class-Content/10-OOP/01-Activities/02-Stu_Constructors/Unsolved/index.js
@@ -0,0 +1,6 @@
+// TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+// TODO: Include a method called 'introduction()'
+
+// TODO: Create a new object using the 'Developer' constructor
+
+// TODO: Call the 'introduction()' method on the new object
