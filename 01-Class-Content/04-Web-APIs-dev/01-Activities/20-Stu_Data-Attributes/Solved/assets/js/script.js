94c77a8a5c2a8a29d3a94440a4faf3041b33e082
sevans924 <sevans924@gmail.com>
2020-12-01 16:26:05 -0500

    PR Changes

--- 01-Class-Content/04-Web-APIs-dev/01-Activities/20-Stu_Data-Attributes/Solved/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs-dev/01-Activities/20-Stu_Data-Attributes/Solved/assets/js/script.js
@@ -6,11 +6,11 @@ container.addEventListener("click", function(event) {
   if (element.matches(".box")) {
     var state = element.getAttribute("data-state");
 
-    // Use a if statement to conditionally render the number on the card
+    // Use an if statement to conditionally render the number on the card
     if (state === "hidden") {
       // If the card is clicked while the state is "hidden", we set .textContent to the number 
       element.textContent = element.dataset.number;
-      // Use dataset property we change the state to visible because the user can now see the number
+      // Using the dataset property, we change the state to visible because the user can now see the number
       element.dataset.state = "visible";
    
     } else {
