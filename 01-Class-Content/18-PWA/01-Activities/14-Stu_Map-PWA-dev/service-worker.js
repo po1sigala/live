65ac898feea9143d4d55a468b0af3ab2c691ddf3
markjnkim <markjnkim@gmail.com>
2021-01-14 22:33:36 -0800

    add new student activity 14 for students

---
+++ 01-Class-Content/18-PWA/01-Activities/14-Stu_Map-PWA-dev/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('static').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './assets/css/style.css',
+        './assets/images/logo128.png',
+        './assets/images/map.jpg',
+      ]);
+    })
+  );
+  console.log('Install');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
