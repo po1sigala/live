1078e93fe9f7f369be7cde30dd3286f66fe2894a
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-15 17:24:11 -0700

    merge

--- 01-Class-Content/09-NodeJS/01-Activities/06-Stu_Let-Const/Unsolved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/06-Stu_Let-Const/Unsolved/index.js
@@ -1,49 +1,48 @@
-
 // TODO: Fix the declarations so that the function works properly.
 const addGreetingMessage = (name) => {
-  var message = "How are you?"
+  var message = 'How are you?';
 
-  if(name.length > 0){
-      var message = "Hello " + name
-      console.log(message)
+  if (name.length > 0) {
+    var message = 'Hello ' + name;
+    console.log(message);
   }
 
-  console.log(message)
-}
+  console.log(message);
+};
 
-addGreetingMessage('Tammy')
+addGreetingMessage('Tammy');
 
 // TODO: Fix the declarations so that the function works properly.
 const calloutCounter = () => {
-  const callout = 'Outside of the loop'
-  var counter = 5
+  const callout = 'Outside of the loop';
+  var counter = 5;
 
-  while( counter > 0) {
-      var callout = 'Inside the loop';
-      console.log(counter, callout)
-      counter--
+  while (counter > 0) {
+    var callout = 'Inside the loop';
+    console.log(counter, callout);
+    counter--;
   }
 
   console.log(callout);
-}
+};
 
-calloutCounter()
+calloutCounter();
 
 // TODO: Fix the declarations so that the function works properly.
 const countMatrix = (matrix) => {
-  for (var index = 0; index < matrix.length; index++) {
-    var line = matrix[index];
-    for (var index = 0; index < line.length; index++) {
-      var element = line[index];
+  for (var i = 0; i < matrix.length; i++) {
+    var line = matrix[i];
+    for (var i = 0; i < line.length; i++) {
+      var element = line[i];
       console.log(element);
     }
   }
-}
+};
 
 var matrix = [
   [1, 2, 3],
   [4, 5, 6],
-  [7, 8, 9]
+  [7, 8, 9],
 ];
 
 countMatrix(matrix);
