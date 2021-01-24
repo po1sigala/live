13981f2351a6749bd8bc6697c537a10b671d2313
markjnkim <markjnkim@gmail.com>
2021-01-24 07:19:03 -0800

    change background color for PWA, add comments for service worker event handlers remove manifest icons from service worker cache modify lesson plan demo and review

--- 01-Class-Content/18-PWA/01-Activities/14-Stu_Map-PWA-dev/Solved/service-worker.js
+++ 01-Class-Content/18-PWA/01-Activities/14-Stu_Map-PWA-dev/Solved/service-worker.js
@@ -1,26 +1,25 @@
-self.addEventListener('install', (event) => {
+// install event handler
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('static').then((cache) => {
+    caches.open('static').then( cache => {
       return cache.addAll([
         './',
         './index.html',
         './assets/css/style.css',
-        './assets/images/logo16.png',
-        './assets/images/logo48.png',
-        './assets/images/logo128.png',
-        './assets/images/logo512.png',
         './assets/images/brandenburg.jpg',
         './assets/images/reichstag.jpg',
-        './assets/images/map.jpg'
+        './assets/images/map.jpg',
       ]);
     })
   );
   console.log('Install');
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+// retrieve assets from cache
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then( response => {
       return response || fetch(event.request);
     })
   );
