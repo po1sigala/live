6be2fc152b53bd86e76b94167f13ee987e33a0a4
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-01-29 17:03:07 -0700

    forgot to format

--- 01-Class-Content/06-Server-Side-APIs/01-Activities/13-Ins_Fetch_Status/assets/js/script.js
+++ 01-Class-Content/06-Server-Side-APIs/01-Activities/13-Ins_Fetch_Status/assets/js/script.js
@@ -5,12 +5,12 @@ var responseText = document.getElementById('response-text');
 function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
-      console.log(response)
-        if(response.status === 200){
-          responseText.textContent = response.status
-        }
-        return response.json()
-    })
+      console.log(response);
+      if (response.status === 200) {
+        responseText.textContent = response.status;
+      }
+      return response.json();
+  });
 }
 
 getApi(requestUrl);
