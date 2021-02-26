88e21078abfa1235d1f54679f2a5527fa56ca62f
Xandromus <xanderrapstine@gmail.com>
2021-02-26 11:19:54 -0600

    remove class content for Units 13-14

--- 01-Class-Content/13-MVC/01-Activities/15-CallbackReview/03.js
+++
@@ -1,9 +0,0 @@
-const subtract = (numOne, numTwo) => numOne - numTwo;
-
-const multiply = (numOne, numTwo) => numOne * numTwo;
-
-const addCallBacks = (functionOne, functionTwo) =>
-  functionOne(6, 2) + functionTwo(6, 3);
-
-// What does this return?
-addCallBacks(subtract, multiply);
--- 01-Class-Content/14-Full-Stack/01-Activities/13-Post-Author-Association/Unsolved/models/author.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Author = sequelize.define('Author', {
-    // Giving the Author model a name of type STRING
-    name: DataTypes.STRING,
-  });
-  return Author;
-};
