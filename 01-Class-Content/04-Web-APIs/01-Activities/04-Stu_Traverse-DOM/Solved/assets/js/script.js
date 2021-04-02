1a3f98ba3ee91dedb3f3b3840ca71db0bff1c8c8
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-04-02 14:29:33 -0400

    fix(1738): fixes 04.4 activity

--- 01-Class-Content/04-Web-APIs/01-Activities/04-Stu_Traverse-DOM/Solved/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/04-Stu_Traverse-DOM/Solved/assets/js/script.js
@@ -1,7 +1,7 @@
 // Access element using id
-var articlesDiv = document.getElementById("articles");
-var mainDiv = document.getElementById("main");
+var articlesDiv = document.getElementById('articles');
+var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[0].style.fontSize = "50px";
-mainDiv.children[1].style.color = "white";
+articlesDiv.children[0].style.fontSize = '50px';
+headerDiv.children[1].style.color = 'white';
--- 01-Class-Content/04-Web-APIs/01-Activities/04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
@@ -1,7 +1,7 @@
 // Access element using id
-var articlesDiv = document.getElementById("articles");
-var mainDiv = document.getElementById("main");
+var articlesDiv = document.getElementById('articles');
+var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[2].style.fontSize = "50px";
-mainDiv.style.color = "white";
+articlesDiv.children[2].style.fontSize = '50px';
+headerDiv.style.color = 'white';
