fc7067159a5340151267be91a0c20ab3bc28663f
--list <jmccamb@gmail.com>
2021-04-29 16:35:28 -0500

    revert change to query selector

--- 01-Class-Content/14-MVC/01-Activities/10-Stu_Handlebars-FE-Logic/Solved/public/js/add-dish.js
+++ 01-Class-Content/14-MVC/01-Activities/10-Stu_Handlebars-FE-Logic/Solved/public/js/add-dish.js
@@ -4,7 +4,7 @@ async function newFormHandler(event) {
   const dish_name = document.querySelector('#dish_name').value;
   const description = document.querySelector('#description').value;
   const guest_name = document.querySelector('#guest_name').value;
-  const has_nuts = document.querySelector('#has_nuts').checked;
+  const has_nuts = document.querySelector('#has_nuts:checked') ? true : false
 
   const response = await fetch(`/api/dish`, {
     method: 'POST',
