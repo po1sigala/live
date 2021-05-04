c18968260b7c8760976fc4badfd70f977958954c
--list <jmccamb@gmail.com>
2021-05-04 10:00:47 -0500

    Inital commit:
    - remove HTML CSS
    - update song list to current songs
    - make for of loop embed positions based on their index positions

--- 01-Class-Content/09-NodeJS/01-Activities/22-Stu_for-of/Solved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/22-Stu_for-of/Solved/index.js
@@ -1,5 +1,18 @@
-const songs = document.querySelectorAll("ul > li");
+const green = (text) => console.log(`\x1b[36m${text}\x1b[0m`);
 
-for (const song of songs) {
-  song.classList.add("green");
+const songs = [
+  'MONTERO (Call Me By Your Name)',
+  'Peaches (feat. Daniel Caesar & Giveon)',
+  'Kiss Me More (feat. SZA)',
+  'Astronaut In The Ocean',
+  'Save Your Tears (with Ariana Grande) (Remix)',
+  'RAPSTAR',
+  'Levitating (feat. DaBaby)',
+  'Leave The Door Open',
+  'Botella Tras Botella',
+  'Fiel',
+];
+
+for (const [index, song] of songs.entries()) {
+  green(`${index + 1}: ${song}`);
 }
