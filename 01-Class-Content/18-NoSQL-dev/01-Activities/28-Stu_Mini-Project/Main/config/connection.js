26ca4d97be7f9ca4391a5e1d126b036254a2b95a
--list <jmccamb@gmail.com>
2021-06-02 14:47:52 -0500

    Add mongodb connection string env variable for heroku

--- 01-Class-Content/18-NoSQL-dev/01-Activities/28-Stu_Mini-Project/Main/config/connection.js
+++ 01-Class-Content/18-NoSQL-dev/01-Activities/28-Stu_Mini-Project/Main/config/connection.js
@@ -1,6 +1,9 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/studentsDB', {
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost/studentsDB';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
