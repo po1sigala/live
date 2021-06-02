dae0071bc71d4c187aacd94553adbc901ceeaf0b
--list <jmccamb@gmail.com>
2021-06-02 12:47:42 -0500

    Fix index.js

--- 01-Class-Content/18-NoSQL-dev/01-Activities/28-Stu_Mini-Project/Main/index.js
+++ 01-Class-Content/18-NoSQL-dev/01-Activities/28-Stu_Mini-Project/Main/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const seed = require('./utils/seed');
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -7,8 +6,6 @@ const cwd = process.cwd();
 
 const PORT = 3001;
 const app = express();
-
-seed();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('01-Activities')
