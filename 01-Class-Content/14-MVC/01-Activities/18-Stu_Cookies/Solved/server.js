38f5a8d20df6457416729a4e8b4c9b0352b82468
--list <jmccamb@gmail.com>
2021-06-08 11:37:03 -0500

    Refactor activity 18 to reflect accurate usage of cookies in express session

--- 01-Class-Content/14-MVC/01-Activities/18-Stu_Cookies/Solved/server.js
+++ 01-Class-Content/14-MVC/01-Activities/18-Stu_Cookies/Solved/server.js
@@ -15,8 +15,17 @@ const PORT = process.env.PORT || 3001;
 // Sets up session and connect to our Sequelize db
 const sess = {
   secret: 'Super secret secret',
-  // Tells our session to use cookies
-  cookie: {},
+  // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
+  cookie: {
+    // maxAge sets the maximum age for the session to be active. Listed in milliseconds.
+    maxAge: 3600,
+    // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+    httpOnly: true,
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+    secure: false,
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   // Sets up session store
@@ -39,5 +48,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
 });
