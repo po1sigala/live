fd225877c0d04644c994f5c6da4b7e1f075a9931
Tamar <mischegoss@gmail.com>
2021-07-14 17:53:40 -0400

    Add 12.3

--- 01-Class-Content/12-SQL/01-Activities/23-Ins_COUNT-GROUPBY/db/queries.sql
+++ 01-Class-Content/12-SQL/01-Activities/23-Ins_COUNT-GROUPBY/db/queries.sql
@@ -2,12 +2,9 @@ SELECT *
 FROM course_names;
 
 SELECT department, COUNT(id) AS number_courses
-FROM course_names; 
-
-SELECT department, COUNT(id) AS number_courses
 FROM course_names
 GROUP BY department;
 
-SELECT department, SUM(total_enrolled) AS total_enrolled
+SELECT department, SUM(total_enrolled) AS sum_enrolled
 FROM course_names
 GROUP BY department;
