1095df96961c977f37769c699b45bd94ab1ecb37
John McCambridge <jmccamb@gmail.com>
2021-07-26 17:42:23 -0500

    update activit 14

--- 01-Class-Content/18-NoSQL-dev/01-Activities/14-Stu_CRUD-Mongoose/Unsolved/server.js
+++ 01-Class-Content/18-NoSQL-dev/01-Activities/14-Stu_CRUD-Mongoose/Unsolved/server.js
@@ -10,50 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Creates a new department
-app.post('/new-department/:department', function(req, res, err) {
+app.post('/new-department/:department', function (req, res, err) {
   const newDepartment = new Department({ name: req.params.department });
   newDepartment.save();
   if (newDepartment) {
     res.json(newDepartment);
   } else {
-    console.log("Uh Oh, something went wrong");
+    console.log('Uh Oh, something went wrong');
     res.json(err);
   }
 });
 
 // Finds all documents
-app.get('/all-departments', function(req, res) {
-  Department.find({}, function(err, result) {
-    if (err) throw err;
+app.get('/all-departments', function (req, res) {
+  Department.find({}, function (err, result) {
     if (result) {
       res.json(result);
-    } else {
-      console.log("Uh Oh, something went wrong");
-      res.json(err);
-    }
-  });
-});
-
-// Finds first instance of document that meets criteria, in this case with the name "Kids"
-app.get('/find-one-department', function(req, res) {
-  Department.findOne({name: "Kids"}, function(err, result) {
-    if (err) throw err;
-    if (result) {
-      res.json(result)
-    } else {
-      console.log("Uh Oh, something went wrong");
-      res.json(err);
-    }
-  });
-});
-
-// Finds one department provided in URL param and deletes
-app.delete('/find-one-delete/:department', function(req, res) {
-  Department.findOneAndDelete({ name: req.params.department }, function (err, result) {
-    if (err) throw err;
-    if (result) {
-      res.json(result);
-      console.log('Deleted: ' + result);
     } else {
       console.log('Uh Oh, something went wrong');
       res.json(err);
@@ -61,9 +33,37 @@ app.delete('/find-one-delete/:department', function(req, res) {
   });
 });
 
-app.post('/find-one-update/:department', function(req, res) {
- // TODO: Write a route that will find the first instance of a document that contains a name with the value equal to 'Kids'
- // Update that name with the value given from the URL param
+// Finds first instance of document that meets criteria, in this case with the name "Kids"
+app.get('/find-one-department', function (req, res) {
+  Department.findOne({ name: 'Kids' }, function (err, result) {
+    if (result) {
+      res.json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.json(err);
+    }
+  });
+});
+
+// Finds one department provided in URL param and deletes
+app.delete('/find-one-delete/:department', function (req, res) {
+  Department.findOneAndDelete(
+    { name: req.params.department },
+    function (err, result) {
+      if (result) {
+        res.json(result);
+        console.log(`Deleted: ${result}`);
+      } else {
+        console.log('Uh Oh, something went wrong');
+        res.json(err);
+      }
+    }
+  );
+});
+
+app.post('/find-one-update/:department', function (req, res) {
+  // TODO: Write a route that will find the first instance of a document that contains a name with the value equal to 'Kids'
+  // Update that name with the value given from the URL param
 });
 
 db.once('open', () => {
