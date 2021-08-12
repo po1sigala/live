5671061ca9ed7923fe4b4ace9cc1733c2cb0caf2
gchoi2u <gchoi@2u.com>
2021-08-11 21:30:43 -0400

    added comments to Unsolved files for 12-Stu

--- 01-Class-Content/18-NoSQL-dev/01-Activities/12-Stu_Models-Schemas/Unsolved/server.js
+++ 01-Class-Content/18-NoSQL-dev/01-Activities/12-Stu_Models-Schemas/Unsolved/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
+// TODO: Add a comment describing the functionality of the code below
 const { Book } = require('./models');
 
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/all-books', (req, res) => {
+  // TODO: Add a comment describing the functionality of the code below
   Book.find({}, (err, result) => {
     if (err) {
       res.status(500).send({ message: 'Internal Server Error' });
