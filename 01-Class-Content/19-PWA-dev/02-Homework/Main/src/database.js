88b06ec93e96228dc59b4c8f14b7991b73b482ae
John McCambridge <jmccamb@gmail.com>
2021-08-23 11:00:20 -0500

    add db file

---
+++ 01-Class-Content/19-PWA-dev/02-Homework/Main/src/database.js
@@ -0,0 +1,7 @@
+const localData = localStorage.getItem('content');
+
+const database = () => {
+  // database logic for IDB
+}
+
+export database;
--- 01-Class-Content/19-PWA-dev/02-Homework/Main/src/index.js
+++ 01-Class-Content/19-PWA-dev/02-Homework/Main/src/index.js
@@ -1,5 +1,6 @@
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
+import { database } from './database';
 import './css/style.css';
 
 const main = document.querySelector('#main');
