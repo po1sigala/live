5b7f1815017dd753ec8c2f75594f80ceb9f8188e
John McCambridge <jmccamb@gmail.com>
2021-08-25 17:30:25 -0500

    Resolving merge conflicts

--- 01-Class-Content/19-PWA-dev/02-Homework/Main/client/src/js/header.js
+++ 01-Class-Content/19-PWA-dev/02-Homework/Main/client/src/js/header.js
@@ -9,8 +9,4 @@ just another text editor
 */                          
 `;
 
-<<<<<<< HEAD
 export { header };
-=======
-export { header };
->>>>>>> 3039f2473 (Merge branch 'BCCOD-1754-Unit-19-HW-Build' of https://github.com/coding-boot-camp/fullstack-ground into BCCOD-1754-Unit-19-HW-Build)
