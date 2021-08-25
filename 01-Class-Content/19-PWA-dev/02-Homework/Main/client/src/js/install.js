d62344d6a2af0a34ad90f28402d0b26ca912c7fe
John McCambridge <jmccamb@gmail.com>
2021-08-25 17:25:29 -0500

    fix linting errors

--- 01-Class-Content/19-PWA-dev/02-Homework/Main/client/src/js/install.js
+++ 01-Class-Content/19-PWA-dev/02-Homework/Main/client/src/js/install.js
@@ -1,26 +1,24 @@
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall');
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
-  });
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
 
 butInstall.addEventListener('click', async () => {
-  
   const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
-   return;
+    return;
   }
 
   promptEvent.prompt();
-  
+
   window.deferredPrompt = null;
-  
+
   butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-  
   window.deferredPrompt = null;
 });
