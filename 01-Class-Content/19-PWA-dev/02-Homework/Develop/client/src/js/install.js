e5e6ee22f842cfd4dcb12d030b588243a8906f25
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-08-26 09:26:55 -0700

    removed more

--- 01-Class-Content/19-PWA-dev/02-Homework/Develop/client/src/js/install.js
+++ 01-Class-Content/19-PWA-dev/02-Homework/Develop/client/src/js/install.js
@@ -1,24 +1,15 @@
 const butInstall = document.getElementById('buttonInstall');
 
+// Add the logic to install a PWA.
+
 window.addEventListener('beforeinstallprompt', (event) => {
-  window.deferredPrompt = event;
-  butInstall.classList.toggle('hidden', false);
+  
 });
 
 butInstall.addEventListener('click', async () => {
-  const promptEvent = window.deferredPrompt;
-
-  if (!promptEvent) {
-    return;
-  }
-
-  promptEvent.prompt();
-
-  window.deferredPrompt = null;
-
-  butInstall.classList.toggle('hidden', true);
+ 
 });
 
 window.addEventListener('appinstalled', (event) => {
-  window.deferredPrompt = null;
+  
 });
--- 01-Class-Content/19-PWA-dev/02-Homework/Develop/client/webpack.config.js
+++ 01-Class-Content/19-PWA-dev/02-Homework/Develop/client/webpack.config.js
@@ -3,6 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// Complete the webpack configuration
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,50 +17,12 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E',
-        description: 'Takes notes with JavaScript syntax highlighting!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
+      
     ],
 
     module: {
       rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
-        },
+        
       ],
     },
   };
