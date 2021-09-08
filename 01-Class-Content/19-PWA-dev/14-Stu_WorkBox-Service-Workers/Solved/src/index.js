8389610b6022c96b2690c423b47de1c2d71ab315
John McCambridge <jmccamb@gmail.com>
2021-09-08 13:31:54 -0500

    Add base app for activity 14 solved"

---
+++ 01-Class-Content/19-PWA-dev/14-Stu_WorkBox-Service-Workers/Solved/src/index.js
@@ -0,0 +1,38 @@
+import './css/style.css';
+
+const targetEl = document.querySelector('.flexbox');
+console.log('🚀 - file: index.js - line 4 - targetEl', targetEl);
+// get the articles
+async function getDevArticles() {
+  const response = await fetch('https://dev.to/api/articles');
+  const articles = await response.json();
+  return articles;
+}
+
+getDevArticles().then((articles) => {
+  document.getElementById('articles').innerHTML = '';
+  articles.forEach((article) => {
+    const articleEl = document.createElement('article');
+
+    articleEl.innerHTML = `
+    <div class="article-container">
+      <div class="row">
+        <div class="col s12">
+          <div class="card blue-grey darken-1">
+            <div class="card-image">
+              <img src="${article.cover_image || ''}">
+            </div>
+            <div class="card-content white-text">
+              <span class="card-title">${article.title}</span>
+              <p>${article.description}</p>
+            </div>
+            <div class="card-action">
+              <a href="${article.url}">Read more!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    targetEl.appendChild(articleEl);
+  });
+});
