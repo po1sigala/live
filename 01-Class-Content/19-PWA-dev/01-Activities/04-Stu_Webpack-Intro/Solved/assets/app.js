681166bbc553ec983626a3cdc0a60403babe6f44
markjnkim <markjnkim@gmail.com>
2021-09-12 23:34:23 -0700

    remove modular front scripts

--- 01-Class-Content/19-PWA-dev/01-Activities/04-Stu_Webpack-Intro/Solved/assets/app.js
+++ 01-Class-Content/19-PWA-dev/01-Activities/04-Stu_Webpack-Intro/Solved/assets/app.js
@@ -1,45 +1,23 @@
-const { mortgagePayment } = require('./mortgage');
-const convertInterest = require('./convertInterest');
-const { months } = require('./monthlyTerms');
-
 const loanEl = document.getElementById('loan');
 const rateEl = document.getElementById('rate');
 const termEl = document.getElementById('term');
 const monthlyPaymentEl = document.getElementById('monthly-payment');
 const submitBtn = document.getElementById('submit');
 
-// const
 function submit(e) {
   e.preventDefault();
   const principle = parseInt(loanEl.value, 10);
-  console.log(principle);
 
   const rate = parseFloat(rateEl.value);
-  // const monthlyRate = convertInterest.monthlyInterest(rate);
   const monthlyRate = rate / 12 / 100;
-  console.log(monthlyRate);
 
   const term = parseInt(termEl.value, 10);
-  // const payNum = months(term);
   const payNum = term * 12;
 
-  // const monthlyPay = mortgagePayment(principle, monthlyRate, payNum);
-
   const ratePow = (1 + rate) ** payNum;
-
   const monthlyPayment = (principle * monthlyRate * ratePow) / (ratePow - 1);
 
-  monthlyPaymentEl.innerText = monthlyPayment;
-  console.log(monthlyPayment);
-
-
-
-  // const total = mortgage.mortgagePayment(
-  //   Number(balanceEl.innerText),
-  //   priceEl.value
-  // );
-  // balanceEl.innerText = total;
-  // addToList(expenseEl.value, priceEl.value);
+  monthlyPaymentEl.innerText = `$${monthlyPayment}`;
 }
 
 submitBtn.onclick = submit;
