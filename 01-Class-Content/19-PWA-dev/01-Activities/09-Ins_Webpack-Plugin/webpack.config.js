75c5cc2185a3dd46133cfde17e28ced3b6142548
markjnkim <markjnkim@gmail.com>
2021-09-14 09:19:08 -0700

    remove old instructor 09 demo

--- 01-Class-Content/19-PWA-dev/01-Activities/09-Ins_Webpack-Plugin/webpack.config.js
+++ 01-Class-Content/19-PWA-dev/01-Activities/09-Ins_Webpack-Plugin/webpack.config.js
@@ -2,16 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: './assets/js/app.js',
+  entry: './src/js/app.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  // Add plugin property in an array
   plugins: [
-    // This plugin uses the index.html and outputs the HTML file with the bundles inserted
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './index.html', // Uses the index.html as the template
     }),
   ],
 };
