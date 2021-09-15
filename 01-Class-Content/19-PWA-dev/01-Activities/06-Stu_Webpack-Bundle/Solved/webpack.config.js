4c15d4bb2ef897212d91c649832af57d9a19db52
markjnkim <markjnkim@gmail.com>
2021-09-15 08:21:43 -0700

    add page in Stu Solved 06

--- 01-Class-Content/19-PWA-dev/01-Activities/06-Stu_Webpack-Bundle/Solved/webpack.config.js
+++ 01-Class-Content/19-PWA-dev/01-Activities/06-Stu_Webpack-Bundle/Solved/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 const config = {
-  entry: './assets/app.js',
+  entry: {
+    main: './assets/app.js',
+    cost: './assets/cost.js',
+  },
   output: {
     filename: '[name].bundle.js', // Dynamically named bundles
     path: path.resolve(__dirname, 'dist'),
