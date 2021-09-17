c14ed68b9e28097fdc67464af40fbb1b9c2aa206
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2021-09-17 16:42:28 -0700

    Clean up

--- 01-Class-Content/19-PWA-dev/01-Activities/22-Stu_IndexedDB/Solved/webpack.config.js
+++ 01-Class-Content/19-PWA-dev/01-Activities/22-Stu_IndexedDB/Solved/webpack.config.js
@@ -1,7 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
--- 01-Class-Content/19-PWA-dev/01-Activities/22-Stu_IndexedDB/Unsolved/webpack.config.js
+++ 01-Class-Content/19-PWA-dev/01-Activities/22-Stu_IndexedDB/Unsolved/webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
