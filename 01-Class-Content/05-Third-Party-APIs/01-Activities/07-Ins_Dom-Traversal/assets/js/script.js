c772f8ad82345684a76a29862efbbfe582b384cb
markjnkim <markjnkim@gmail.com>
2021-09-17 10:54:37 -0700

    add turquoise to highlight

--- 01-Class-Content/05-Third-Party-APIs/01-Activities/07-Ins_Dom-Traversal/assets/js/script.js
+++ 01-Class-Content/05-Third-Party-APIs/01-Activities/07-Ins_Dom-Traversal/assets/js/script.js
@@ -1,11 +1,11 @@
 // Open this file in the browser and display the console in DevTools.
 
 // Starting from the `<main id="top">` element, place a blue border around the title of the website, Develop Community Center
-// Uncomment the following two lines to demonstrate the effect and the log.
-$('#top').children().css('border', 'red solid 2px');
-console.log($('#top').children();
+// Comment out the following two lines to demonstrate the before and after effect on the webpage. 
+$('#top').children().css('border', 'turquoise solid 2px');
+console.log($('#top').children());
 
-$('#top').children().first().addClass('p-5').css('border', '#1bbc9b solid 2px');
+// $('#top').children().first().addClass('p-5').css('border', '#1bbc9b solid 2px');
 
 // $('#top').children().eq(5).css('border', 'purple solid 2px');
 
