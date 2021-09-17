99c42f524d9d99d7b44bd985d832ca714cf6846b
markjnkim <markjnkim@gmail.com>
2021-09-17 07:46:21 -0700

    simplify the methods

--- 01-Class-Content/05-Third-Party-APIs/01-Activities/07-Ins_Dom-Traversal/assets/js/script.js
+++ 01-Class-Content/05-Third-Party-APIs/01-Activities/07-Ins_Dom-Traversal/assets/js/script.js
@@ -1,9 +1,12 @@
-// Open this file in the browser and display the console
+// Open this file in the browser and display the console in DevTools.
 
 // Starting from the `<main id="top">` element, place a blue border around the title of the website, Develop Community Center
 // Uncomment the following two lines to demonstrate the effect and the log.
-// $('#top').children().css('border', 'red solid 2px');
-// console.log($('#top').children();
+$('#top').children().css('border', 'red solid 2px');
+console.log($('#top').children();
+
+$('#top').children().first().addClass('p-5').css('border', '#1bbc9b solid 2px');
+
 // $('#top').children().eq(5).css('border', 'purple solid 2px');
 
 // Starting from `<main id="top">`, add an li element to the list, ul.
