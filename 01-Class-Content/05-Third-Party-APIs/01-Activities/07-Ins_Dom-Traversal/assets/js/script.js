8f66cfcd7fa6dc4a7f261d678625f4b37f6124c0
markjnkim <markjnkim@gmail.com>
2021-09-25 06:09:33 -0700

    instructor demo

--- 01-Class-Content/05-Third-Party-APIs/01-Activities/07-Ins_Dom-Traversal/assets/js/script.js
+++ 01-Class-Content/05-Third-Party-APIs/01-Activities/07-Ins_Dom-Traversal/assets/js/script.js
@@ -1,12 +1,12 @@
 // Open this file in the browser and display the console in DevTools.
 
-// Starting from the `<main id="top">` element, place a blue border around the title of the website, Develop Community Center
-$('#top').children().css('border', 'aqua dashed 3px');
+// Highlight which elements in the DOM are the children of the parent element
+// $('#top').children().css('color', 'yellow');
 console.log($('#top').children());
 
-$('#top').children().eq(0).addClass('p-5').css('border', 'aqua solid 2px');
+// $('#top').children().eq(0).css('text-decoration', 'underline');
 
 // Starting from `<main id="top">`, add an li element to the list, ul.
 
 // Starting from `<main id="top">`, add the classes to all of its child `<li>` elements
-$('#top').children().eq(4).children().css('background-color', 'aqua');
+// $('#top').children().eq(4).children().css('background-color', 'aqua');
