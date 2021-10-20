e75eff25db0b56b7ca0b10f2735fbb6924b43955
John McCambridge <jmccamb@gmail.com>
2021-10-20 09:09:59 -0500

    Fix populate method

--- 01-Class-Content/21-MERN/01-Activities/09-Ins_Mutations/schemas/resolvers.js
+++ 01-Class-Content/21-MERN/01-Activities/09-Ins_Mutations/schemas/resolvers.js
@@ -3,28 +3,28 @@ const { School, Class, Professor } = require('../models');
 const resolvers = {
   Query: {
     schools: async () => {
-      return await School.find({}).populate('classes').populate({
+      await School.find({}).populate('classes').populate({
         path: 'classes',
-        populate: 'professor'
+        populate: 'professor',
       });
     },
     classes: async () => {
-      return await Class.find({}).populate('professor');
+      await Class.find({}).populate('professor');
     },
     class: async (parent, args) => {
-      return await Class.findById(args.id);
+      await Class.findById(args.id).populate('professor');
     },
     professors: async () => {
-      return await Professor.find({}).populate('classes');
-    }
+      await Professor.find({}).populate('classes');
+    },
   },
   // Define the functions that will fulfill the mutations
   Mutation: {
     addSchool: async (parent, { name, location, studentCount }) => {
       // Create and return the new School object
-      return await School.create({ name, location, studentCount });
-    }
-  }
+      await School.create({ name, location, studentCount });
+    },
+  },
 };
 
 module.exports = resolvers;
