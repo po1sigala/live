e1af60c084657252a9694c04164da7e3f669a68f
gchoi2u <62606072+gchoi2u@users.noreply.github.com>
2021-10-21 10:27:25 -0400

    Update 01-Class-Content/10-OOP/02-Homework/Main/index.js
    Co-authored-by: Tamar Auber <mischegoss@gmail.com>

--- 01-Class-Content/10-OOP/02-Homework/Main/index.js
+++ 01-Class-Content/10-OOP/02-Homework/Main/index.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const DIST_DIR = path.resolve(__dirname, "dist")
+const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "team.html");
 
 const render = require("./src/page-template.js");
