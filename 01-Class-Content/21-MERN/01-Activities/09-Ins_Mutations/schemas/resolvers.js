35eb3a469d4723e3d65dc66f2e8d408fa2ae8146
John McCambridge <jmccamb@gmail.com>
2021-10-25 23:11:50 -0500

    Fix return statements

--- 01-Class-Content/21-MERN/01-Activities/09-Ins_Mutations/schemas/resolvers.js
+++ 01-Class-Content/21-MERN/01-Activities/09-Ins_Mutations/schemas/resolvers.js
@@ -3,26 +3,26 @@ const { School, Class, Professor } = require('../models');
 const resolvers = {
   Query: {
     schools: async () => {
-      await School.find({}).populate('classes').populate({
+      return await School.find({}).populate('classes').populate({
         path: 'classes',
         populate: 'professor',
       });
     },
     classes: async () => {
-      await Class.find({}).populate('professor');
+      return await Class.find({}).populate('professor');
     },
     class: async (parent, args) => {
-      await Class.findById(args.id).populate('professor');
+      return await Class.findById(args.id).populate('professor');
     },
     professors: async () => {
-      await Professor.find({}).populate('classes');
+      return await Professor.find({}).populate('classes');
     },
   },
   // Define the functions that will fulfill the mutations
   Mutation: {
     addSchool: async (parent, { name, location, studentCount }) => {
       // Create and return the new School object
-      await School.create({ name, location, studentCount });
+      return await School.create({ name, location, studentCount });
     },
   },
 };
