7e407ca8b3f9c38dd69adac8c17fe8615e186140
Tamar Auber <mischegoss@gmail.com>
2021-11-19 17:59:21 -0500

    Update config and Mongoose package

--- 01-Class-Content/18-NoSQL/02-Homework/Main/config/connection.js
+++ 01-Class-Content/18-NoSQL/02-Homework/Main/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia', {
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://localhost:27017/socialmedia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
+// Export connection
 module.exports = mongoose.connection;
