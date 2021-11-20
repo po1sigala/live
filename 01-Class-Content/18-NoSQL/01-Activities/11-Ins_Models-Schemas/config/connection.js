b7c2e8e7d1cb447276374a597f2f7d69af90122f
Tamar Auber <mischegoss@gmail.com>
2021-11-20 11:29:37 -0500

    Add updated Mongoose to 11-12 and Mini-Project

--- 01-Class-Content/18-NoSQL/01-Activities/11-Ins_Models-Schemas/config/connection.js
+++ 01-Class-Content/18-NoSQL/01-Activities/11-Ins_Models-Schemas/config/connection.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mygroceryDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+})
 
 // Export connection 
 module.exports = mongoose.connection;
