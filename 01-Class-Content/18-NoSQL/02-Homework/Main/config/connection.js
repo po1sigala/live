4b8af39602f450c59f57940f2b287045270b7e4d
Tamar Auber <mischegoss@gmail.com>
2021-11-21 18:43:10 -0500

    Fix localhost on Main/HW

--- 01-Class-Content/18-NoSQL/02-Homework/Main/config/connection.js
+++ 01-Class-Content/18-NoSQL/02-Homework/Main/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-// Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://localhost:27017/socialmedia', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmedia', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
-// Export connection
 module.exports = mongoose.connection;
