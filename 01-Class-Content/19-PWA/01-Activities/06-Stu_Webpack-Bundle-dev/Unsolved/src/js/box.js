33d5bbe705eb4b27f367cf02b5159f871e3b35a5
gchoi2u <gchoi@2u.com>
2021-12-06 10:48:04 -0500

    fixed small typo

--- 01-Class-Content/19-PWA/01-Activities/06-Stu_Webpack-Bundle-dev/Unsolved/src/js/box.js
+++ 01-Class-Content/19-PWA/01-Activities/06-Stu_Webpack-Bundle-dev/Unsolved/src/js/box.js
@@ -1,10 +1,10 @@
-// TODO import images as Blue and Yellow
+// TODO: import images as Blue and Yellow
 
 export const boxClick = () => {
-  const box = document.getElementById("box");
+  const box = document.getElementById('box');
   if (box.src === Yellow) {
     box.src = Blue;
   } else {
     box.src = Yellow;
   }
-}
+};
--- 01-Class-Content/19-PWA/01-Activities/06-Stu_Webpack-Bundle-dev/Unsolved/src/js/index.js
+++ 01-Class-Content/19-PWA/01-Activities/06-Stu_Webpack-Bundle-dev/Unsolved/src/js/index.js
@@ -1,10 +1,9 @@
-import { boxClick } from "./box";
-import {headerClick} from "./header";
-import '../css/style.css'
-// TODO import images as Blue and Yellow
+import { boxClick } from './box';
+import { headerClick } from './header';
+import '../css/style.css';
+// TODO: import images as Blue and Yellow
 
-document.getElementById("boxBtn").addEventListener("click", boxClick);
-document.getElementById("headerBtn").addEventListener("click", headerClick);
+document.getElementById('boxBtn').addEventListener('click', boxClick);
+document.getElementById('headerBtn').addEventListener('click', headerClick);
 
-// TODO add image to #box element src
-
+// TODO: add image to #box element src
