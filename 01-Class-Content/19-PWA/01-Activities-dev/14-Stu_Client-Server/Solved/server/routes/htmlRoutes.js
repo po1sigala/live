d23745553f3f8ebda1550e783cc579d64e7d7804
Tamar Auber <mischegoss@gmail.com>
2022-01-03 00:27:00 -0500

    Add comments to 13-14

--- 01-Class-Content/19-PWA/01-Activities-dev/14-Stu_Client-Server/Solved/server/routes/htmlRoutes.js
+++ 01-Class-Content/19-PWA/01-Activities-dev/14-Stu_Client-Server/Solved/server/routes/htmlRoutes.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-// TODO: Add comment explaining role of route below
+// This route path will match requests to the root route / and sends the index.html file in the client's dist folder 
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
