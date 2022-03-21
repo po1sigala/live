208be75f3b15959b4d132b4cc7acf8e9621bdead
Tamar Auber <mischegoss@gmail.com>
2022-03-21 17:44:33 -0400

    Rever bad path

--- 01-Class-Content/22-State/01-Activities/14-Stu_React-Router-Review/Solved/server/server.js
+++ 01-Class-Content/22-State/01-Activities/14-Stu_React-Router-Review/Solved/server/server.js
@@ -16,11 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
