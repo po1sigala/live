e51a4f2b3ce59bc4feabe6418bf81641efdaa7d8
Tamar Auber <mischegoss@gmail.com>
2022-04-20 11:49:39 -0400

    Add challenge copy from Blended

--- 01-Class-Content/19-PWA/01-Activities/26-Stu_Manifest/Unsolved/assets/js/index.js
+++ 01-Class-Content/19-PWA/01-Activities/26-Stu_Manifest/Unsolved/assets/js/index.js
@@ -81,7 +81,7 @@ fetchList();
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // Register workbox service worker
-  const workboxSW = new Workbox('/service-worker.js');
+  const workboxSW = new Workbox('./service-worker.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
