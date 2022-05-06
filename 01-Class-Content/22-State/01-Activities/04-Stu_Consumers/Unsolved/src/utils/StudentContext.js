f410fe221da0989be825d78e13eac20bfb5f2087
Carlos Costoso <ccostoso@c02gq142q05q.2tor.net>
2022-05-06 13:32:16 -0400

    Fixes Activity 04

--- 01-Class-Content/22-State/01-Activities/04-Stu_Consumers/Unsolved/src/utils/StudentContext.js
+++ 01-Class-Content/22-State/01-Activities/04-Stu_Consumers/Unsolved/src/utils/StudentContext.js
@@ -27,7 +27,23 @@ export const StudentProvider = ({ children }) => {
     if (!student.name) {
       return;
     }
-    const id = students.length + 1;
+    // Create id variable for new student
+    let id;
+
+    // If there are any entries in the students array already
+    if (students.length) {
+      // Create array of all ids currently assigned to students
+      const ids = students.map((s) => s.id);
+
+      // Sort array so that highest number is last
+      ids.sort((a, b) => a - b);
+
+      // Store highest number, plus 1, in id variable
+      id = ids[ids.length - 1] + 1;
+    } else {
+      // If students array is empty, assign new student an id of 1
+      id = 1;
+    }
 
     // We use the spread operator to fill in the details from the student object that was passed while adding the new `id`
     const newStudent = { ...student, id };
