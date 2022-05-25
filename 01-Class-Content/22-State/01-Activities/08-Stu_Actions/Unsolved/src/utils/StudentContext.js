c6b4cd3c6e5f80e88af31f1df0cc7a4e742751d4
Carlos Costoso <ccostoso@c02gq142q05q.2tor.net>
2022-05-25 15:19:00 -0400

    Adds Module 22, Activity 08 createId function

--- 01-Class-Content/22-State/01-Activities/08-Stu_Actions/Unsolved/src/utils/StudentContext.js
+++ 01-Class-Content/22-State/01-Activities/08-Stu_Actions/Unsolved/src/utils/StudentContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
+import createId from './createId';
 
 // Initialize new context for students
 const StudentContext = createContext();
@@ -23,17 +24,7 @@ export const StudentProvider = ({ children }) => {
 
   // Function to add a student
   const addStudent = (student) => {
-    let id;
-
-    if (students.length) {
-      const ids = students.map((s) => s.id);
-
-      ids.sort((a, b) => a - b);
-
-      id = ids[ids.length - 1] + 1;
-    } else {
-      id = 1;
-    }
+    const id = createId(students);
 
     // We use the spread operator to fill in the details from the student object that was passed while adding the new `id`
     const newStudent = { ...student, id };
