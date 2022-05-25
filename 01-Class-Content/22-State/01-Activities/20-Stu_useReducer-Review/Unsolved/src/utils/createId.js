bd778cce0e55a80bb98768daaaf1166006d66091
Carlos Costoso <ccostoso@c02gq142q05q.2tor.net>
2022-05-25 15:26:03 -0400

    Adds Module 22, Activity 10 createId function

---
+++ 01-Class-Content/22-State/01-Activities/20-Stu_useReducer-Review/Unsolved/src/utils/createId.js
@@ -0,0 +1,17 @@
+const createId = (students) => {
+  let id;
+
+  if (students.length) {
+    const ids = students.map((s) => s.id);
+
+    ids.sort((a, b) => a - b);
+
+    id = ids[ids.length - 1] + 1;
+  } else {
+    id = 1;
+  }
+
+  return id;
+};
+
+export default createId;
