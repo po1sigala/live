7c03b1c0ebf99e822a523aface46dd2953132ca1
John Desrosiers <john.desro@gmail.com>
2022-07-01 11:16:30 -0700

    Add comments to mod06/act06/Solved
    This issue fixes #2506 by adding comments to the solution for
    activity 06 in module 06.

--- 01-Class-Content/06-Server-Side-APIs/01-Activities/06-Stu_Reverse_APIs/Solved/assets/js/script.js
+++ 01-Class-Content/06-Server-Side-APIs/01-Activities/06-Stu_Reverse_APIs/Solved/assets/js/script.js
@@ -1,6 +1,10 @@
 var requestUrl = 'https://api.github.com/orgs/Netflix/repos';
 
-// JQuery AJAX
+// Send an API request using the JQuery ajax method. On completion, the callback
+// function passed to .then() will be called. $.ajax makes things a bit easier
+// for developers by parsing JSON data before passing it to the callback
+// function. This means that JSON.parse does not need to be used to begin using
+// the data.
 $.ajax({
   url: requestUrl,
   method: 'GET',
@@ -9,9 +13,16 @@ $.ajax({
   console.log(response);
 });
 
-// Browser Fetch Method
+// Send an API request using the built-in browser fetch method. Fetch passes a
+// response object when calling back the function passed to .then(). Unlike
+// $.ajax, this response object is more thant just the data. The body of the
+// response must be parsed by using one the methods for parsing the response. In
+// this example, the .json() method is used because the GitHub API response uses
+// JSON for the format.
 fetch(requestUrl)
   .then(function (response) {
+    // In order to use the data, it must first be parsed. Use .json() when the
+    // API response format is JSON.
     return response.json();
   })
   .then(function (data) {
@@ -19,13 +30,25 @@ fetch(requestUrl)
     console.log(data);
   });
 
-// Browser XMLHttpRequest
+// Send an API request using the built-in browser XMLHttpRequest object.
+// This API is much older than fetch and appeared in very early browsers like IE.
+// Construct an instance of XMLHttpRequest.
 var xhr = new XMLHttpRequest();
+// Assign a callback function which will be run any time the state of the request changes.
 xhr.onreadystatechange = function () {
+  // The readyState property is a number and  will be equal to DONE once the
+  // entire response has been received.
   if (xhr.readyState === XMLHttpRequest.DONE) {
     console.log('XMLHttpRequest Response \n-------------');
+    // Access the response is a string in this case because the GitHub API
+    // responds with JSON data. In order to use the data, it will need to be
+    // converted into a JavaScript Array using JSON.parse().
     console.log(xhr.response);
   }
 };
+
+// Initialize the request
 xhr.open('GET', requestUrl);
+
+// Asynchronously send the request to the GitHub API server.
 xhr.send();
