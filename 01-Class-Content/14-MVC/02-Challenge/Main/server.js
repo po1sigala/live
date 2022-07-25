da8fab23315776a626db0b645da6e264aafcc039
Carlos Costoso <ccostoso@c02gq142q05q.2tor.net>
2022-07-25 12:24:47 -0400

    Removes accidental change in module 14 Challenge server.js file

--- 01-Class-Content/14-MVC/02-Challenge/Main/server.js
+++ 01-Class-Content/14-MVC/02-Challenge/Main/server.js
@@ -12,12 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 3600,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
