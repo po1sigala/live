6126b773f98cef0315aabb791c55e705b3ec8511
Carlos Costoso <ccostoso@c02gq142q05q.2tor.net>
2022-08-04 12:29:28 -0400

    Adds maxage to server cookie for activities/challenge in mod14

--- 01-Class-Content/14-MVC/02-Challenge/Main/server.js
+++ 01-Class-Content/14-MVC/02-Challenge/Main/server.js
@@ -12,7 +12,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 86400,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
